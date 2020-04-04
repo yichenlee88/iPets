341386e05d044f75fbfd8bfba00325f8c6239588
YichenLee <10646004@ntub.edu.tw>
2020-04-05 00:27:49 +0800

    update about.vue

---
+++ iPets/src/components/About.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="container mt-3">
+    <ul class="nav nav-tabs nav-fill" style="margin-top: 20px; color: #e3f2fd;">
+      <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#motivation">專案動機</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#public">給大眾</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#special">特別</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#member">組員介紹</a>
+      </li>
+    </ul>
+    <br />
+    <div class="tab-content">
+      <div class="tab-pane fade show active con-heigh" id="motivation">
+        <p class="lead">
+          <br />
+          <br />你可從狗狗的動作了解牠的情緒。雖然你可能不是很懂狗狗的肢體語言 (當然流口水的意思顯而易見)，但你確實需要了解如何解讀狗狗的行為。你是否留意到你的愛犬一直舔著某種材質，或是圍繞你面前的一個特定位置不斷轉圈圈？如果狗狗有情緒或健康方面的問題，很多原因都會導致上述行為。只要留心觀察毛小孩的行為，就能即時給予協助。你可從狗狗的動作了解牠的情緒。雖然你可能不是很懂狗狗的肢體語言 (當然流口水的意思顯而易見)，但你確實需要了解如何解讀狗狗的行為。你是否留意到你的愛犬一直舔著某種材質，或是圍繞你面前的一個特定位置不斷轉圈圈？如果狗狗有情緒或健康方面的問題，很多原因都會導致上述行為。只要留心觀察毛小孩的行為，就能即時給予協助。
+        </p>
+      </div>
+      <div class="tab-pane fade con-heigh" id="public">
+        <p class="lead">
+          <br />
+          <br />如果狗狗轉圈圈轉個不停，牠可能是有健康方面的問題。沒錯，狗有時喜歡追著自己的尾巴玩，但如果毛小孩已經到失控的程度，就可能有點問題了。耳朵感染可能造成繞圈行為，而牛頭梗可能會出現追逐尾巴的強迫症行為如果狗狗轉圈圈轉個不停，牠可能是有健康方面的問題。沒錯，狗有時喜歡追著自己的尾巴玩，但如果毛小孩已經到失控的程度，就可能有點問題了。耳朵感染可能造成繞圈行為，而牛頭梗可能會出現追逐尾巴的強迫症行為
+        </p>
+      </div>
+      <div class="tab-pane fade con-heigh" id="special">
+        <p class="lead">
+          <br />
+          <br />狗狗在地面挖掘的原因有很多種：想要逃脫、為了追蹤動物、想做個舒服好躺的窩，或是為了藏重要的東西。但是，有些毛小孩也有在室內「挖坑」的習慣。你有沒有看過愛犬不停刮抓著地毯或沙發，好發現愜意的休息地點？這種行為最常發生在夜間和毛小孩的小睡時間，算是相當正常的行為。狗狗在地面挖掘的原因有很多種：想要逃脫、為了追蹤動物、想做個舒服好躺的窩，或是為了藏重要的東西。但是，有些毛小孩也有在室內「挖坑」的習慣。你有沒有看過愛犬不停刮抓著地毯或沙發，好發現愜意的休息地點？這種行為最常發生在夜間和毛小孩的小睡時間，算是相當正常的行為。
+        </p>
+      </div>
+      <div class="tab-pane fade con-heigh" id="member">
+        <div class="card-deck">
+        <div class="row col-12">
+          <div class="card col-4" style="margin-bottom:10px">
+            <img src="../assets/04.jpg" alt="Avatar" style="width:100%" />
+            <div class="container con-text">
+              <h4>
+                <b>李怡蓁</b>
+              </h4>
+              <a class="nav-link con-link" href="https://github.com/yichenlee88">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+          <div class="card col-4" style="margin-bottom:10px">
+            <img src="../assets/43.jpg" alt="Avatar" style="width:100%" />
+            <div class="container con-text">
+              <h4>
+                <b>蔡欣恬</b>
+              </h4>
+              <a class="nav-link con-link" href="https://github.com/watson050308">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+          <div class="card col-4" style="margin-bottom:10px">
+            <img src="../assets/05.jpg" alt="Avatar" style="width:100%" />
+            <div class="container con-text">
+              <h4>
+                <b>黃琳恩</b>
+              </h4>
+              <a class="nav-link con-link" href="https://github.com/Linen3082">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="row col-12">
+          <div class="card col-4">
+            <img src="../assets/14.jpg" alt="Avatar" style="width:100%" />
+            <div class="container con-text">
+              <h4>
+                <b>王偉霖</b>
+              </h4>
+              <a class="nav-link con-link" href="https://github.com/n1066414">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+          <div class="card col-4">
+            <img src="../assets/23.jpg" alt="Avatar" style="width:100%" />
+            <div class="container con-text">
+              <h4>
+                <b>劉芳汝</b>
+              </h4>
+              <a class="nav-link con-link" href="https://github.com/lululiu29783638">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-4"></div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+
+<style>
+.con-heigh {
+  min-height: 600px;
+  text-shadow: 0 0 1px gray;
+  width: 80%;
+  margin: 0 auto;
+  margin-bottom: 15px;
+  text-align: left;
+  font-family: "Microsoft JhengHei", "sans-serif";
+  line-height: 45px;
+  display: inline-block;
+  align-items: center;
+}
+
+.con-text{
+     size:12px;
+     text-align: center;
+     vertical-align: middle;
+     margin-top: 10px;
+}
+
+.con-link {
+    color:black;
+    margin-top: -10px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 30%;
+  border-radius: 5px;
+  margin-top: 20px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+
+img {
+  margin-top: 10px;
+  border-radius: 5px 5px 5px 5px;
+}
+</style>
