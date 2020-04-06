b6728df5bd8fb642c68565325011bd289ed72421
YichenLee <10646004@ntub.edu.tw>
2020-04-07 00:45:27 +0800

    update login.vue
    暫定版本

--- iPets/src/components/Login.vue
+++ iPets/src/components/Login.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="container">
+    <img src="../assets/logo_banner.png" class="banner_png" />
     <div class="card">
-        <div class="card-header" style="background-color: #e3f2fd;">登入</div>
-        <div class="card-body">
-            <div class="row col-12">
-                <div class="col-2">
-                    <label class="txtEmail">電子郵件: </label>
-                </div>
-                <div class="col-10">
-                    <input type="email" style="margin-top: 15px;" class="form-control" name="txtEmail" id="txtEmail" required placeholder="請輸入信箱">
-                </div>
+      <div class="card-header" style="background-color: #e3f2fd;">登入</div>
+      <div class="card-body">
+        <input
+          type="email"
+          style="margin-top: 15px;"
+          class="form-control"
+          name="txtEmail"
+          id="txtEmail"
+          placeholder="請輸入信箱"
+        />
+        <input
+          class="form-control"
+          style="margin-top: 15px;"
+          name="txtPwd"
+          id="txtPwd"
+          placeholder="輸入密碼"
+        />
+        <input
+          class="btn"
+          type="submit"
+          value="送出"
+          style="margin-top: 15px; background-color: #e3f2fd; width: 100%"
+        />
+        <div class="hide-md-lg">
+          <p>或者</p>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a href="#" class="fb btn">
+                    <i class="fa fa-facebook fa-fw"></i> Login with Facebook
+                </a>
             </div>
-            <label class="txtEmail">電子郵件: </label>
-            <input type="email" style="margin-top: 15px;" class="form-control" name="txtEmail" id="txtEmail" required placeholder="請輸入信箱">
-            <label class="txtPwd" style="margin-top: 15px;">密碼：</label>
-            <input class="form-control" style="margin-top: 15px;" name="txtPwd" id="txtPwd" required placeholder="輸入密碼">
-            <input class="btn" type="submit" value="送出" style="margin-top: 15px; background-color: #e3f2fd; width: 100%">
-            <div class="hide-md-lg">
-                <p>Or sign with:</p>
+            <div class="col-12">
+                <a href="#" class="twitter btn">
+                    <i class="fa fa-twitter fa-fw"></i> Login with Twitter
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="#" class="google btn">
+                    <i class="fa fa-google fa-fw"></i> Login with Google+
+                </a>
             </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,24 +61,62 @@ export default {
 
 <style scoped>
 .container {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: 0px;
-    font-family: "Microsoft JhengHei", "sans-serif";
-    width: 70%;
-    min-height: 600px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 0px;
+  font-family: "Microsoft JhengHei", "sans-serif";
+  width: 40%;
+  min-height: 600px;
+}
+
+.banner_png {
+  width: 60%;
+  margin: auto;
 }
 
 .row {
-    vertical-align: bottom;
-    line-height: 50px;
-    text-align: center;
-    display: row;
+  vertical-align: bottom;
+  line-height: 50px;
+  text-align: center;
+}
+
+.btn {
+  width: 100%;
+  padding: 12px;
+  border: none;
+  border-radius: 4px;
+  margin: 5px 0;
+  opacity: 0.85;
+  display: inline-block;
+  font-size: 17px;
+  line-height: 20px;
+  text-decoration: none;
+  opacity: inherit;
+}
+
+input:hover,
+.btn:hover {
+  opacity: 1;
 }
 
 .hide-md-lg {
-    font-family: "Microsoft JhengHei", "sans-serif";
-    text-align: center;
-    margin-top: 15px;
+  font-family: "Microsoft JhengHei", "sans-serif";
+  text-align: center;
+  margin-top: 15px;
+}
+
+.fb {
+  background-color: #3B5998;
+  color: white;
+}
+
+.twitter {
+  background-color: #55ACEE;
+  color: white;
+}
+
+.google {
+  background-color: #dd4b39;
+  color: white;
 }
 </style>
