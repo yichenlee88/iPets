e7019fd9855c3edd866f363c2d7d1c7f9cd5f1e7
n1066414 <n1066414@ntub.edu.tw>
2020-04-28 02:20:21 +0800

    add sign in function

--- app/iPetsTest/app/src/main/java/com/example/ipetstest/SignInFragment.java
+++ app/iPetsTest/app/src/main/java/com/example/ipetstest/SignInFragment.java
@@ -1,11 +1,16 @@
 package com.example.ipetstest;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.widget.Button;
 
 import android.view.LayoutInflater;
@@ -13,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -40,7 +46,7 @@ public class SignInFragment extends Fragment {
         sent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ReadSingleContact();
+                ReadSingleContact(view);
             }
         });
     }
@@ -53,15 +59,14 @@ public class SignInFragment extends Fragment {
     }
 
 
-    private void ReadSingleContact() {
+    private void ReadSingleContact(final View view) {
 
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
-        EditText edaccount = getView().findViewById(R.id.account);
-        EditText edpassword = getView().findViewById(R.id.password);
+        final EditText edaccount = getView().findViewById(R.id.account);
+        final EditText edpassword = getView().findViewById(R.id.password);
         final TextView textView = getView().findViewById(R.id.textView4);
         String account = edaccount.getText().toString();
-        String password = edpassword.getText().toString();
-        textView.setText(account);
+        final String password = edpassword.getText().toString();
         DocumentReference user = db.collection("userInformation").document(account);
         user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -70,8 +75,28 @@ public class SignInFragment extends Fragment {
                     DocumentSnapshot doc = task.getResult();
                     StringBuilder fields = new StringBuilder("");
                     fields.append(doc.get("password"));
-
-                    textView.setText(fields.toString());
+                    String query = fields.toString();
+                    textView.setText(query);
+                    if (query.equals(password)){
+                        NavController controller = Navigation.findNavController(view);
+                        controller.navigate(R.id.action_signInFragment_to_singInSuccessFragment);
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle("登入失敗");
+                        builder.setMessage("基帳號或密碼錯誤,大小血虛相符");
+                        builder.setNegativeButton("重新輸入",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                // TODO Auto-generated method stub
+                                edaccount.setText("");
+                                edpassword.setText("");
+                            }
+                        });
+                        builder.setCancelable(true);   //设置按钮是否可以按返回键取消,false则不可以取消
+                        AlertDialog dialog = builder.create();  //创建对话框
+                        dialog.setCanceledOnTouchOutside(true);      //设置弹出框失去焦点是否隐藏,即点击屏蔽其它地方是否隐藏
+                        dialog.show();
+                    }
                 }
             }
         })
@@ -83,10 +108,7 @@ public class SignInFragment extends Fragment {
                 });
 
     }
-    public void login(){
 
-
-    }
 
     
 
