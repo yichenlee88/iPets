20bc9c6a5fd5d2e15a328d36818b1c3d9a72455d
Linen3082 <love5032121@gmail.com>
2020-05-01 11:38:36 +0800

    update map location

--- app/iPetsTest/app/src/main/java/com/example/ipetstest/MapsActivity.java
+++ app/iPetsTest/app/src/main/java/com/example/ipetstest/MapsActivity.java
@@ -1,19 +1,51 @@
 package com.example.ipetstest;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.collect.Maps;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static int LOCATION_PERMISSION_REQUEST_CODE = 1001;
+    private static boolean rLocationGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +60,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng latLng = new LatLng(25.042260, 121.525331);
+        float zoom = 17;
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("我在這!");
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        mMap.addMarker(markerOptions);
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(25.041913, 121.525536);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void PetSuppliesStore(View view) {
+        LatLng latLng2 = new LatLng(25.055901, 121.514937);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng2));
+    }
+
+
+    public void PetGroomingShop(View view) {
+        LatLng latLng3 = new LatLng(25.056931, 121.516418);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng3));
+    }
+
+    public void PetHospital(View view) {
+        LatLng latLng4 = new LatLng(25.056173, 121.517341);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng4));
+    }
+
+    public void PetHotel(View view) {
+        LatLng latLng5 = new LatLng(25.057602, 121.515120);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng5));
     }
 }
+
