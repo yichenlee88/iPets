8dc8be3fe36114fe3e2af841d600e57b645fcc36
YichenLee <10646004@ntub.edu.tw>
2020-05-01 20:15:06 +0800

    register user account to firebase's console by Email and Password

--- iPets/src/router/index.js
+++ iPets/src/router/index.js
@@ -1,39 +1,45 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/Home'
-import About from '@/components/About'
-import DogInfo from '@/components/DogInfo'
-import Contact from '@/components/Contact'
-import Login from '@/components/Login'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import About from "@/components/About";
+import DogInfo from "@/components/DogInfo";
+import Contact from "@/components/Contact";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'About',
+      path: "/about",
+      name: "About",
       component: About
     },
     {
-      path: '/dogInfo',
-      name: 'DogInfo',
+      path: "/dogInfo",
+      name: "DogInfo",
       component: DogInfo
     },
     {
-      path: '/contact',
-      name: 'Contact',
+      path: "/contact",
+      name: "Contact",
       component: Contact
     },
     {
-      path: '/login',
-      name: 'Login',
+      path: "/login",
+      name: "Login",
       component: Login
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register
     }
   ]
-})
+});
