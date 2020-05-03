f954d0bf8593b475e4269d111ae30d84bf121fcd
n1066423 <lululiu.29783638@gmail.com>
2020-05-04 02:19:27 +0800

    modify homeFragment.java,loginFragment.java,color.xml,string.xml

--- app/iPets/app/src/main/java/com/example/ipets/homeFragment.java
+++ app/iPets/app/src/main/java/com/example/ipets/homeFragment.java
@@ -1,13 +1,22 @@
 package com.example.ipets;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 /**
@@ -15,11 +24,31 @@ import android.view.ViewGroup;
  */
 public class homeFragment extends Fragment {
 
-
     public homeFragment() {
         // Required empty public constructor
     }
 
+    /*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setContentView(R.layout.fragment_home);
+        BottomNavigationView bottomNavigationView = getView().findViewById(R.id.nav_bottom);
+        Fragment home = getFragmentManager().findFragmentById(R.id.homeFragment);
+        Fragment analyze = getFragmentManager().findFragmentById(R.id.analyzeFragment);
+        bottomNavigationView.setOnNavigationItemSelectedListener((item) -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    home.getFragmentManager().findFragmentById(R.id.homeFragment);
+                    break;
+                case R.id.analyze:
+                    analyze.getFragmentManager().findFragmentById(R.id.analyzeFragment);
+                    break;
+            }
+            return true;
+        });
+    }
+     */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,5 +56,7 @@ public class homeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
 
 }
