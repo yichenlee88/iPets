f87bbce9a7c1176babb00e83cc2dff812f14d644
YichenLee <10646004@ntub.edu.tw>
2020-05-07 02:07:16 +0800

    modify navbar and modify home
    之後會統一修改路徑名稱

--- iPets/src/components/HomeLogin.vue
+++ iPets/src/components/HomeLogin.vue
@@ -1,5 +1,46 @@
 <template>
-  <h2>登入成功</h2>
+  <div class="container">
+    <div
+      class="accordion md-accordion accordion-blocks"
+      id="accordionEx78"
+      role="tablist"
+      aria-multiselectable="true"
+    >
+      <div class="card">
+        <div class="card-header" role="tab" id="headingUnfiled">
+          <div class="dropdown float-left">
+            <button
+              class="btn btn-info btn-sm m-0 mr-3 p-2 dropdown-toggle"
+              type="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i class="fas fa-pencil-alt"></i>
+            </button>
+            <div class="dropdown-menu dropdown-info">
+              <a class="dropdown-item" href="#">新增</a>
+              <a class="dropdown-item" href="#">刪除</a>
+              <a class="dropdown-item" href="#">修改</a>
+            </div>
+          </div>
+
+          <a
+            data-toggle="collapse"
+            data-parent="#accordionEx78"
+            href="#collapseUnfiled"
+            aria-expanded="true"
+            aria-controls="collapseUnfiled"
+          >
+            <h5 class="mt-1 mb-0">
+              <span>洗澡</span>
+              <i class="fas fa-angle-down rotate-icon"></i>
+            </h5>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +48,10 @@ export default {
   name: "HomeLogin"
 };
 </script>
+
+<style scoped>
+.container {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+</style>
