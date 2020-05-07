d77e60aa650d80e778fd04104fd91092d79dc3da
xintian <watson050308@gmail.com>
2020-05-07 20:35:51 +0800

    create dogScience

---
+++ iPets/src/components/dogScience.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h2>狗狗科普</h2><br><br>
+    <div id="section1" class="row my-3">
+      <div class="col-lg-8 columnberight">
+        <div class="media">
+          <div class="media-body">
+            <img src="https://static.ettoday.net/images/598/d598355.jpg" width="500" height="281" alt>
+            <a href="https://pets.ettoday.net/news/353044" target="_blank">
+              <h3>狗狗吃大便是天性？了解原因及改善方法</h3></a>
+                <p>
+                  每當看到狗狗吃大便時，主人們都會覺得不衛生且噁心，便對狗狗又罵又打，但其實狗狗吃大便是一件很正常的反應行為，將大便吃掉不僅可以保持清潔，還可以防止味道外洩，避免其他動物捕獵，保護自身安全。 
+                </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dogScience",
+  data() {
+    return {};
+  }
+};
+</script>
