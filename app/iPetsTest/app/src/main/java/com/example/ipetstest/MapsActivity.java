c52b765754b32089d9cca2e298f31d04f79ccf06
Linen3082 <love5032121@gmail.com>
2020-05-11 16:53:26 +0800

    add MapsActivity.java
    iPetTest : add search location funtion(add search icon pictures)；change marker color

--- app/iPetsTest/app/src/main/java/com/example/ipetstest/MapsActivity.java
+++ app/iPetsTest/app/src/main/java/com/example/ipetstest/MapsActivity.java
@@ -6,11 +6,19 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.slice.SliceItem;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -21,12 +29,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
@@ -37,10 +50,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static boolean rLocationGranted = false;
     private FusedLocationProviderClient mFusedLocationProvider;
 
+    ArrayList<LatLng> arrayList=new ArrayList<LatLng>();
+    LatLng PetSuppliesStore1 = new LatLng(25.062097, 121.525432);
+    LatLng PetSuppliesStore2 = new LatLng(25.031245, 121.529331);
+    LatLng PetSuppliesStore3 = new LatLng(25.028951, 121.538968);
+    LatLng PetSuppliesStore4 = new LatLng(25.035700, 121.532458);
+
+    LatLng PetGroomingShop1 = new LatLng(25.057657, 121.523878);
+    LatLng PetGroomingShop2 = new LatLng(25.038043, 121.529479);
+    LatLng PetGroomingShop3 = new LatLng(25.049152, 121.525224);
+    LatLng PetGroomingShop4 = new LatLng(25.034107, 121.545246);
+
+
+    LatLng PetHospital1 = new LatLng(25.043239, 121.525051);
+    LatLng PetHospital2 = new LatLng(25.043171, 121.528863);
+    LatLng PetHospital3 = new LatLng(25.047033, 121.531570);
+    LatLng PetHospital4 = new LatLng(25.036919, 121.532993);
+
+    LatLng PetHotel1 = new LatLng(25.032257, 121.516281);
+    LatLng PetHotel2 = new LatLng(25.034057, 121.543851);
+    LatLng PetHotel3 = new LatLng(25.033744, 121.537176);
+    LatLng PetHotel4 = new LatLng(25.043526, 121.543569);
+
+    //widgets
+    private EditText mSearchText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_maps);
+
+        mSearchText = (EditText) findViewById(R.id.input_search);
+
         //如果true則初始化Map
         if(chkPlayService()==true) {
             initialMap();
@@ -49,8 +90,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.map);
                 mapFragment.getMapAsync(this);
+
+                arrayList.add(PetSuppliesStore1);
+                arrayList.add(PetSuppliesStore2);
+                arrayList.add(PetSuppliesStore3);
+                arrayList.add(PetSuppliesStore4);
+
+                arrayList.add(PetGroomingShop1);
+                arrayList.add(PetGroomingShop2);
+                arrayList.add(PetGroomingShop3);
+                arrayList.add(PetGroomingShop4);
+
+                arrayList.add(PetHospital1);
+                arrayList.add(PetHospital2);
+                arrayList.add(PetHospital3);
+                arrayList.add(PetHospital4);
+
+                arrayList.add(PetHotel1);
+                arrayList.add(PetHotel2);
+                arrayList.add(PetHotel3);
+                arrayList.add(PetHotel4);
             }
         }
+        init();
+    }
+
+    private void init(){
+        Log.d("init", "init : Initializing");
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+                    //execute our method  for searching
+                    geoLocate();
+                }
+                return false;
+            }
+        });
     }
 
     //建立初始化Map的方法
@@ -83,12 +163,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         if(task.isSuccessful()){
                             //找到位置
                             Location mLocation = (Location) task.getResult();
-                            LatLng latLng = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here!");
-                            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
-                            mMap.addMarker(markerOptions);
+                            LatLng nowlatLng = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+                            MarkerOptions markerOptions = new MarkerOptions().position(nowlatLng).title("I am here!").draggable(true);
+                            mMap.animateCamera(CameraUpdateFactory.newLatLng(nowlatLng));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(nowlatLng, 17));
                             //這個也可以，同上animateCamera功能 : mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                            mMap.getUiSettings().setZoomGesturesEnabled(true);
+                            mMap.getUiSettings().setZoomControlsEnabled(true);
+                            mMap.getUiSettings().setCompassEnabled(true);
+                            mMap.addMarker(markerOptions);
                             Log.i("location","("+mLocation.getLatitude()+", "+mLocation.getLongitude()+") ");
                         }
                     }
@@ -97,6 +180,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         catch (Exception ex){
             Log.e("LocationError",ex.getMessage());
+        }
+    }
+
+    private void geoLocate(){
+        Log.d("geoLocate", "geoLocate : geolocating");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString,1);
+        }catch (IOException e){
+            Log.e("geoLocate", "geoLocate : IOException: " + e.getMessage());
+        }
+        if (list.size() > 0){
+            Address address = list.get(0);
+
+            Log.d("geoLocate", "geoLocate : found a location: " + address.toString());
+
+            LatLng searchlatLng = new LatLng(address.getLatitude(),address.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(searchlatLng));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(searchlatLng, 17));
+            MarkerOptions options = new MarkerOptions().position(searchlatLng);
+            mMap.addMarker(options);
         }
     }
 
@@ -139,31 +247,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void PetSuppliesStore(View view) {
-        LatLng latLng2 = new LatLng(25.055901, 121.514937);
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng2).title("寵物公園用品店");
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng2));
-        mMap.addMarker(markerOptions);
+        for (int i=0;i<arrayList.size();i++){
+            mMap.addMarker(new MarkerOptions().position(arrayList.get(i)).title("Marker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(arrayList.get(i)));
+        }
     }
 
-
     public void PetGroomingShop(View view) {
-        LatLng latLng3 = new LatLng(25.057675, 121.523853);
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng3).title("Wang's Beauty 旺城寵物精品美容");
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng3));
-        mMap.addMarker(markerOptions);
+        for (int i=0;i<arrayList.size();i++){
+            mMap.addMarker(new MarkerOptions().position(arrayList.get(i)).title("Marker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(arrayList.get(i)));
+        }
     }
 
     public void PetHospital(View view) {
-        LatLng latLng4 = new LatLng(25.036141, 121.527525);
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng4).title("恩孺動物診所");
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng4));
-        mMap.addMarker(markerOptions);
+        for (int i=0;i<arrayList.size();i++){
+            mMap.addMarker(new MarkerOptions().position(arrayList.get(i)).title("Marker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(arrayList.get(i)));
+        }
     }
 
     public void PetHotel(View view) {
-        LatLng latLng5 = new LatLng(25.025679, 121.536246);
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng5).title("北歐寵物旅館");
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng5));
-        mMap.addMarker(markerOptions);
+        for (int i=0;i<arrayList.size();i++){
+            mMap.addMarker(new MarkerOptions().position(arrayList.get(i)).title("Marker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(arrayList.get(i)));
+        }
     }
 }
+
