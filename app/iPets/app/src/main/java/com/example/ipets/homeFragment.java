5c8b469f2232d44735089f0bc06294292743b423
lululiu29783638 <lululiu29783638@gmail.com>
2020-05-26 22:32:40 +0800

    create HomeActivity.java
    create activity_home.xml , modify UI in fragment_maps

--- app/iPets/app/src/main/java/com/example/ipets/homeFragment.java
+++ app/iPets/app/src/main/java/com/example/ipets/homeFragment.java
@@ -1,14 +1,19 @@
 package com.example.ipets;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -29,47 +34,18 @@ public class homeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /*
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setContentView(R.layout.fragment_home);
-        BottomNavigationView bottomNavigationView = getView().findViewById(R.id.nav_bottom);
-        Fragment home = getFragmentManager().findFragmentById(R.id.homeFragment);
-        Fragment analyze = getFragmentManager().findFragmentById(R.id.analyzeFragment);
-        bottomNavigationView.setOnNavigationItemSelectedListener((item) -> {
-            switch (item.getItemId()){
-                case R.id.home:
-                    home.getFragmentManager().findFragmentById(R.id.homeFragment);
-                    break;
-                case R.id.analyze:
-                    analyze.getFragmentManager().findFragmentById(R.id.analyzeFragment);
-                    break;
-            }
-            return true;
-        });
-    }
-     */
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Button mapbutton = getView().findViewById(R.id.mapbutton);
-        mapbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NavController controller = Navigation.findNavController(v);
-                controller.navigate(R.id.action_homeFragment_to_mapsActivity);
-            }
-        });
 
     }
+
+
+
 
 
 }
