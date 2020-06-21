6c31294fe723c654e0e66ac8e68efa44628bcb31
xintian <watson050308@gmail.com>
2020-06-21 18:46:53 +0800

    modify manager

--- iPets/src/components/Manager.vue
+++ iPets/src/components/Manager.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-      123
+  <div id="manager">
+    <div id="content">
+      <b-row class="justify-content-md-center" style="margin:20px 0">
+        <b-col cols="8">
+          <b-card bg-variant="light">
+            <b-row class="my-1" v-for="(type,index) in types" :key="index">
+              <b-col sm="3">
+                <label :for="`title-${type.title}`">
+                   <code>{{ type.title }}</code>:
+                </label>
+              </b-col>
+              <b-col sm="9">
+                <b-form-input :id="`type-${type.id}`" :type="type.type"></b-form-input>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "manager"
+  name: "Manager",
+  data() {
+    return {
+      types: [
+        {title: "標題", type: "text", id: "title"},
+        {title: "副標題", type: "text", id: "subtitle"},
+        {title: "日期", type: "date", id: "date"}
+      ]
+    };
+  }
 };
 </script>
-
-<style scoped>
-
-</style>
