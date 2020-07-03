6640342a94b6ac59b43d9e6f061b32e80585bef8
YichenLee <10646004@ntub.edu.tw>
2020-07-03 16:27:10 +0800

    modify footer

--- iPets/src/components/Footer.vue
+++ iPets/src/components/Footer.vue
@@ -1,15 +1,17 @@
 <template>
   <b-container fluid class="footer">
     <b-row>
-      <b-col>1 of 2</b-col>
+      <b-col md="12" class="footer p-4 mb-0"
+        >© 台北商業大學 資訊管理系 版權所有</b-col
+      >
     </b-row>
   </b-container>
 </template>
 
 <style>
 .footer {
-  height: 30px;
-  background-color: #e3f2fd;
-  flex: 0 0 auto;
+  background-color: white;
+  text-align: center;
+  font-size: 18px;
 }
 </style>
