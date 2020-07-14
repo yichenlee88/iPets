acbfeaf9ac56ac6bae31a891ae171bcd52e272b4
xintian <watson050308@gmail.com>
2020-07-15 00:50:50 +0800

    modify dogScience

--- iPets/src/components/dogScience.vue
+++ iPets/src/components/dogScience.vue
@@ -4,8 +4,8 @@
     <br>
     <div class="container marketing">
       <div class="row">
-        <div class="col-12 col-sm-4">
-          <div class="img-thumbnail" style="margin-left: 15px; height: 520px;" v-for="(item, id) in comments" :key="id">
+        <div class="col-12 col-sm-4"  v-for="(item, id) in comments" :key="id">
+          <div class="img-thumbnail" style="margin-left: 15px; height: 520px;">
             <img
               class="rounded"
               :src="item.image"
@@ -16,46 +16,6 @@
             </H4>
             <p>{{ item.introduction }}</p>
             <a href="#/dogScienceArticle" target="_blank">
-              <button
-                type="button"
-                class="btn btn-info"
-                style="position: absolute; right: 20px; bottom: 5px;"
-              >了解更多</button>
-            </a>
-          </div>
-        </div>
-        <div class="col-12 col-sm-4">
-          <div class="img-thumbnail" style="height: 520px;">
-            <img
-              class="rounded"
-              src="https://maoup.com.tw/wp-content/uploads/2020/02/191009_%E5%B0%8F%E7%8B%97%E8%83%961%E5%85%AC%E6%96%A4%E7%AD%89%E6%96%BC%E4%BA%BA%E8%83%9620%E5%85%AC%E6%96%A4.jpg"
-              style="max-width: 100%;"
-            >
-            <H4>
-              <br>驚！小狗胖1公斤，竟等於人胖20公斤？！
-            </H4>
-            <p>根據國外的研究調查，以一隻3公斤的小狗來說，每增重1公斤約等於：160公分的女性增重18公斤！180公分的男性增加22公斤！</p>
-            <a href="https://maoup.com.tw/?p=23482" target="_blank">
-              <button
-                type="button"
-                class="btn btn-info"
-                style="position: absolute; right: 20px; bottom: 5px;"
-              >了解更多</button>
-            </a>
-          </div>
-        </div>
-        <div class="col-12 col-sm-4">
-          <div class="img-thumbnail" style="height: 520px;">
-            <img
-              class="rounded"
-              src="https://maoup.com.tw/wp-content/uploads/2020/03/180802_3-1024x768.png"
-              style="max-width: 100%;ASDF"
-            >
-            <H4>
-              <br>對零食沒興趣的狗狗，該怎麼訓練？
-            </H4>
-            <p>看別人家的汪汪都會乖乖的坐下、握手，怎麼我家的就是學不會呢？別生氣～就像人類小孩也需要因材施教，其實你可能只是沒找到狗狗的心頭好，快跟著毛起來看下去～</p>
-            <a href="https://maoup.com.tw/?p=23571" target="_blank">
               <button
                 type="button"
                 class="btn btn-info"
