6c66b7607fed9065560d6e359c72ffbcdc2da209
YichenLee <10646004@ntub.edu.tw>
2020-07-20 02:16:37 +0800

    modify card and delete code redundant

--- iPets/src/components/About.vue
+++ iPets/src/components/About.vue
@@ -71,209 +71,28 @@
           （三）提供店家資訊：讓用戶可以快速搜尋周遭與寵物有關的店家與醫院，並且可用關鍵字查詢，方便用戶快速找到需要的商家。<br /></p></b-col
     ></b-row>
     <b-row class="MemberTitle text-center"><b-col>組員介紹</b-col> </b-row>
-    <b-row class="MemberContent text-center" cols-lg="3">
+    <b-row class="MemberContent" cols-lg="3">
       <div v-for="(item, i) in post" :key="i">
-        <b-col>
+        <b-col class="MemberCard">
           <b-card
             v-bind:title="item.Name"
             v-bind:img-src="item.src"
             img-alt="Image"
             img-top
             tag="article"
-            style="max-width: 20rem;"
+            align="center"
             class="mb-2"
           >
             <b-card-text>
-              <a
-                class="nav-link con-link"
-                :v-bind:href="item.link"
-                target="_blank"
-              >
-                <i class="fas fa-link"></i>
-              </a>
+              <b-link class="link" :href="item.link"
+                ><i class="fas fa-link"></i
+              ></b-link>
             </b-card-text>
           </b-card>
         </b-col>
       </div>
     </b-row>
   </b-container>
-  <!-- <div class="container mt-3">
-    <ul class="nav nav-tabs nav-fill" style="margin-top: 20px; color: #e3f2fd;">
-      <li class="nav-item">
-        <a class="nav-link active" data-toggle="tab" href="#motivation"
-          >專案動機</a
-        >
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#question">問題與機會</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#special">特別</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#member">組員介紹</a>
-      </li>
-    </ul>
-    <br />
-    <div class="tab-content">
-      <div class="tab-pane fade show active con-heigh" id="motivation">
-        <br />
-        <h5><strong>一、寵物飼養背景與現況</strong></h5>
-        <p>
-          近年來，由於「臺灣的人口結構性變化」和「個人經濟壓力因素」，讓寵物陪伴變成主流，現代的年輕族群結婚後未必會生育孩子，反而會選擇飼養寵物，因為飼養寵物的花費及精力遠遠小於養育孩子，導致現在社會上部分家庭都會飼養寵物，將寵物當成孩子寵愛。
-          現在的社會給年輕族群帶來許多壓力及負擔，當人們感到身心俱疲的時候總會要有一個抒壓、療癒的管道，而寵物的存在便會是最佳首選，當人在極度疲憊的情況下，看到一個可愛的寵物跟你撒嬌，其實疲憊感瞬間可以降低許多，你的喜怒哀樂，都會有一個可愛的寵物與你一起面對。雖然無法和牠言語，但再多的話也無法表達出內心的情緒，陪伴才是最佳消化情緒的辦法。根據農委會調查統計，目前台灣家庭仍以飼養狗類居多，故此次寵物居家管理專題的對象主要針對狗設計。
-        </p>
-        <br />
-        <h5><strong>二、寵物居家管理相關系統背景與現況</strong></h5>
-        <p>
-          目前在手機商店的寵物居家管理程式只有少數幾個，經過本組的觀察後，發現功能幾乎大同小異，而且UI設計風格差不多，整體較無差異性。所以本專題目標是做出不同於市面上且更加舒適的UI，同時具備較便利的管理功能，以及品種辨識技術、商家定位等特殊功能，增加系統差異性及使用者的黏著度。
-          不同於其他寵物居家管理系統只能在手機上使用，本專題開發了手機版及網頁版的管理系統，兩邊系統能夠資料同步與功能相容。除此之外，兩邊系統也會有一些各自獨立的功能，手機版本有定位功能；網頁版本則會有寵物辨識以及寵物圖片風格轉換，結合現今主流趨勢人工智慧，讓本次專題的管理系統更加有趣且豐富。
-        </p>
-        <br />
-      </div>
-      <div class="tab-pane fade con-heigh" id="question">
-        <br />
-        <h6><mark>情況一</mark></h6>
-        <h5>
-          <strong>
-            隨著愈來愈多人以養寵物取代結婚生子，許多青壯年到了適婚年齡，都尚未步入婚姻，就算結婚也沒有計畫生育孩子，在這種情況下，這些家庭便會增加飼養貓或狗的想法。
-          </strong>
-        </h5>
-        <p>
-          「iPets」為這些頂客族、不婚主義者以及正在飼養寵物的飼主，打造了方便且實用的網站及APP，輔助飼主管理寵物的生活，輕鬆養寵物不用擔心忙碌而忘記打理自己的寵物；新手或有想養寵物的人們更是適合，iPets系統有多項功能幫助想養寵物的新手學會如何打理自己的寵物，不用擔心會手忙腳亂。有「iPets」在，讓使用者養寵物不費力、開心輕鬆養寵物。
-        </p>
-        <br />
-        <h6><mark>情況二</mark></h6>
-        <h5>
-          <strong>
-            在這網路發達、高科技的時代，愈來愈少人用手札的方式紀錄行程在行事曆，現代社會人手一機，習慣透過手機行事曆紀錄行程，任何與寵物有關的資訊也會上網查詢而並非閱讀書籍。另外，我們也發現許多飼主會忘記定期打理毛孩子，像是剃毛、驅蟲、打疫苗、洗澡等，這會產生一些影響導致寵物無法健康成長。
-          </strong>
-        </h5>
-        <p>
-          「iPets」有多項提醒功能，像是為寵物剃毛、驅蟲、打疫苗等定期性的事項，使用者可以設定一個週期性於行事曆並開啟提醒功能，讓使用者不必擔心會忘記行程。另外，還具備分析飼料成分的功能，來判斷該飼料是否健康、營養，同時還包括一些用戶專屬的功能，可以協助使用者快速找到寵物醫院、寵物旅館等。
-        </p>
-        <br />
-      </div>
-      <div class="tab-pane fade con-heigh" id="special">
-        <br />
-        <h5><strong>一、簡潔的介面設定</strong></h5>
-        <p>
-          （一）人性化管理介面：操作介面簡潔有力，讓使用者可以一目了然。
-        </p>
-        <h5><strong>二、方便的寵物管理</strong></h5>
-        <p>
-          （一）多寵物管理：假如飼養多隻寵物，只需要註冊該寵物，便可以輕鬆的切換介面查看寵物的行程。<br />
-          （二）行事曆：讓用戶可以記錄寵物的相關行程（如：剃毛、美容等）。<br />
-          （三）倒數計時器：利用簡潔的介面，使用To-Do
-          List，方便管理寵物行程。<br />
-          （四）自動排程：用戶只需設定重複頻率及次數，系統便會自動排程。<br />
-          （五）顏色區別：將較重要的行程以不同顏色表示，讓用戶快速區分活動的先後順序。<br />
-          （六）提醒功能：給予用戶自行設定提醒時間，若未設定，則會在行程到來的前一天及當天通知用戶。
-        </p>
-        <h5><strong>三、豐富使用者體驗</strong></h5>
-        <p>
-          （一）個人化排程：讓使用者自行選擇將哪些倒數計時器顯示於首頁。（如：剃毛、洗澡等）。<br />
-          （二）品種辨識：運用人工智慧技術，將上傳的圖片進行寵物辨識，便可以得知該寵物品種，提升用戶體驗。<br />
-          （三）寵物風格轉換：運用人工智慧技術，將上傳的圖片轉換成五種不同風格的照片，讓用戶對於每次圖片轉換都充滿期待感。<br />
-          （四）好文推薦：過濾假知識，推薦寵物相關文章，給予使用者優質寵物資訊平台。<br />
-          （五）關鍵字搜尋功能：使用者能夠便利地查詢網站所有文章及資訊。<br />
-        </p>
-        <h5><strong>四、提高用戶黏著度</strong></h5>
-        <p>
-          （一）相簿空間：提供用戶存放寵物相片的空間，讓用戶可以將寵物每個瞬間記錄下來，提升用戶使用意願。<br />
-          （二）寵物飼料分析：從網路搜尋相關資訊並經由系統分析，建立可信的資料集，方便用戶作為飼料購買依據，減少選購時間。<br />
-          （三）提供店家資訊：讓用戶可以快速搜尋周遭與寵物有關的店家與醫院，並且可用關鍵字查詢，方便用戶快速找到需要的商家。<br />
-        </p>
-        <br />
-      </div>
-      <div class="tab-pane fade con-heigh" id="member">
-        <div class="card-deck">
-          <br />
-          <div class="row col-12">
-            <div class="card col-4">
-              <img src="../assets/04.jpg" alt="Avatar" style="width:100%" />
-              <div class="container con-text">
-                <h4>
-                  <b>李怡蓁</b>
-                </h4>
-                <a
-                  class="nav-link con-link"
-                  href="https://github.com/yichenlee88"
-                  target="_blank"
-                >
-                  <i class="fas fa-link"></i>
-                </a>
-              </div>
-            </div>
-            <div class="card col-4">
-              <img src="../assets/43.jpg" alt="Avatar" style="width:100%" />
-              <div class="container con-text">
-                <h4>
-                  <b>蔡欣恬</b>
-                </h4>
-                <a
-                  class="nav-link con-link"
-                  href="https://github.com/watson050308"
-                  target="_blank"
-                >
-                  <i class="fas fa-link"></i>
-                </a>
-              </div>
-            </div>
-            <div class="card col-4">
-              <img src="../assets/05.jpg" alt="Avatar" style="width:100%" />
-              <div class="container con-text">
-                <h4>
-                  <b>黃琳恩</b>
-                </h4>
-                <a
-                  class="nav-link con-link"
-                  href="https://github.com/Linen3082"
-                  target="_blank"
-                >
-                  <i class="fas fa-link"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="row col-12" style="margin-bottom: 20px">
-            <div class="card col-4">
-              <img src="../assets/14.jpg" alt="Avatar" style="width:100%" />
-              <div class="container con-text">
-                <h4>
-                  <b>王偉霖</b>
-                </h4>
-                <a
-                  class="nav-link con-link"
-                  href="https://github.com/n1066414"
-                  target="_blank"
-                >
-                  <i class="fas fa-link"></i>
-                </a>
-              </div>
-            </div>
-            <div class="card col-4">
-              <img src="../assets/23.jpg" alt="Avatar" style="width:100%" />
-              <div class="container con-text">
-                <h4>
-                  <b>劉芳汝</b>
-                </h4>
-                <a
-                  class="nav-link con-link"
-                  href="https://github.com/lululiu29783638"
-                  target="_blank"
-                >
-                  <i class="fas fa-link"></i>
-                </a>
-              </div>
-            </div>
-            <div class="col-4"></div>
-            <br />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
@@ -298,14 +117,14 @@ export default {
 .QuestionTitle,
 .SpecialTitle,
 .MemberTitle {
-  font-size: 30px;
+  font-size: 32px;
   line-height: 100px;
 }
 
 .MotivationContent,
 .QuestionContent,
 .SpecialContent {
-  size: 12px;
+  font-size: 18px;
   text-align: center;
   vertical-align: middle;
   margin: 0 auto;
@@ -313,28 +132,10 @@ export default {
   text-align: left;
   font-family: "Microsoft JhengHei", "sans-serif";
   line-height: 40px;
-  width: 85%;
-}
-
-.con-heigh {
-  text-shadow: 0 0 1px gray;
-  width: 85%;
-  margin: 0 auto;
-  margin-bottom: 15px;
-  text-align: left;
-  font-family: "Microsoft JhengHei", "sans-serif";
-  line-height: 40px;
-  display: inline-block;
-  align-items: center;
-}
-
-.con-link {
-  color: black;
 }
 
 .card {
   min-height: 360px;
-  margin-bottom: 20px;
 }
 
 .card:hover {
@@ -345,5 +146,14 @@ export default {
   font-family: "Microsoft JhengHei", Helvetica, Arial, sans-serif;
   margin-top: 10px;
   border-radius: 5px 5px 5px 5px;
+  font-size: 18px;
+}
+
+.MemberCard {
+  margin-bottom: 20px;
+}
+
+.link {
+  color: black;
 }
 </style>
