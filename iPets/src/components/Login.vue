a2aca0ac007a6e149c65b7831f750177fcd2d906
YichenLee <10646004@ntub.edu.tw>
2020-07-25 16:34:10 +0800

    Refactor Login.vue

--- iPets/src/components/Login.vue
+++ iPets/src/components/Login.vue
@@ -11,43 +11,44 @@
       style="width:512px;"
     >
       <b-card-text>
-        <b-row>
-          <b-col>
-            <b-form-input
-              class="InputClass center"
-              id="input-email"
-              v-model="email"
-              placeholder="Email"
-              required
-            ></b-form-input>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col
-            ><b-form-input
-              class="InputClass center"
-              id="input-password"
-              type="password"
-              v-model="password"
-              placeholder="Password"
-              required
-            ></b-form-input
-          ></b-col>
-        </b-row>
-        <!-- <b-row>
-          <b-col col="10">
-            <b-form-input></b-form-input>
+        <b-form-input
+          class="InputClass center"
+          id="input-email"
+          v-model="email"
+          placeholder="Email"
+          required
+        ></b-form-input>
+        <div v-if="passwordHidden">
+          <b-form-input
+            type="password"
+            class="InputClass center"
+            id="input-password"
+            v-model="password"
+            placeholder="Password"
+            required
+          ></b-form-input>
+          <b-input-group-prepend class="mr-n2">
             <span
-              class="display-eye fa fa-eye-slash center"
+              class="display-eye fa fa-eye-slash"
               @click="hidePassword"
-            ></span
-          ></b-col>
-        </b-row> -->
-        <b-row>
-          <b-col>
-            <b-button class="ButtonClass" @click="auth_email">登入</b-button>
-          </b-col>
-        </b-row>
+            ></span>
+          </b-input-group-prepend>
+        </div>
+        <!-- 預設 -->
+        <div v-if="!passwordHidden">
+          <b-form-input
+            class="InputClass center"
+            id="input-password"
+            :type="passwordFieldType"
+            v-model="password"
+            placeholder="Password"
+            required
+          ></b-form-input>
+          <b-input-group-prepend class="mr-n2">
+            <span class="display-eye fa fa-eye" @click="showPassword"></span>
+          </b-input-group-prepend>
+        </div>
+        <b-button class="ButtonClass" @click="auth_email">登入</b-button>
         <b-row
           ><b-col>
             <a class="btn btn-social-icon btn-facebook">
@@ -61,69 +62,6 @@
         ></b-row>
       </b-card-text> </b-card
   ></b-container>
-  <!--
-  <div class="container">
-    <img src="../assets/logo_banner.png" class="center" />
-    <div class="card">
-      <div class="card-header">
-        登入
-      </div>
-      <div class="card-body">
-        <form>
-          <div class="form-group mx-auto my-3" style="width: 80%;">
-            <label for="input-email">電子郵件</label>
-            <input
-              id="input-email"
-              class="form-control"
-              type="email"
-              required
-              placeholder="格式為:xxx@gmail.com"
-              v-model="email"
-            />
-          </div>
-
-          <div class="form-group mx-auto my-3" style="width: 80%;">
-            <label for="input-password">密碼</label>
-            <input
-              id="input-password"
-              class="form-control"
-              type="password"
-              required
-              placeholder="帳號長度為8~12"
-              v-model="password"
-            />
-          </div>
-          <button
-            type="submit"
-            class="w-25 btn btn-primary center"
-            @click="auth_email"
-          >
-            登入
-          </button>
-        </form>
-        <div class="hide-md-lg">
-          <p>或者</p>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <a href="#" class="fb btn">
-              <i class="fa fa-facebook fa-fw"></i> Login with Facebook
-            </a>
-          </div>
-          <div class="col-12">
-            <a href="#" class="twitter btn">
-              <i class="fa fa-twitter fa-fw"></i> Login with Twitter
-            </a>
-          </div>
-          <div class="col-12">
-            <a href="#" class="google btn">
-              <i class="fa fa-google fa-fw"></i> Login with Google+
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
@@ -132,11 +70,23 @@ import { db } from "../db";
 const fAuth = db.auth();
 
 export default {
+  props: {
+    passwordText: {
+      default: "",
+      type: String
+    },
+    fieldLabel: {
+      default: "",
+      type: String
+    }
+  },
   name: "Login",
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      passwordFieldType: "password",
+      passwordHidden: true
     };
   },
   methods: {
@@ -154,6 +104,15 @@ export default {
           console.log("登入成功");
         });
       e.preventDefault();
+    },
+    hidePassword() {
+      this.passwordHidden = false;
+      this.passwordFieldType =
+        this.passwordFieldType === "password" ? "text" : "password";
+    },
+    showPassword() {
+      this.passwordHidden = true;
+      this.passwordFieldType = this.passwordFieldType;
     }
   }
 };
@@ -165,6 +124,24 @@ export default {
   width: 400px;
   border-radius: 40px;
   margin-bottom: 20px;
+}
+
+.InputGroup {
+  height: 48px;
+  width: 400px;
+  border-radius: 40px;
+  margin-bottom: 20px;
+}
+
+.display-eye {
+  position: absolute;
+  height: 24px;
+  width: 24px;
+  top: 46%;
+  margin-top: -6px;
+  right: 70px;
+  z-index: 1;
+  cursor: pointer;
 }
 
 .banner_png {
@@ -196,58 +173,4 @@ export default {
     rgb(26, 201, 228) 100%
   );
 }
-
-/* .btn {
-  width: 80%;
-  padding: 12px;
-  border: none;
-  border-radius: 4px;
-  margin: 5px 0;
-  opacity: 0.85;
-  display: inline-block;
-  font-size: 17px;
-  line-height: 20px;
-  text-decoration: none;
-  opacity: inherit;
-}
-
-input:hover,
-.btn:hover {
-  opacity: 1;
-}
-
-.hide-md-lg {
-  font-family: "Microsoft JhengHei", "sans-serif";
-  text-align: center;
-  margin-top: 15px;
-}
-
-.fb {
-  background-color: #3b5998;
-  color: white;
-}
-
-.twitter {
-  background-color: #55acee;
-  color: white;
-}
-
-.google {
-  background-color: #dd4b39;
-  color: white;
-}
-
-.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 80%;
-}
-
-.card-header {
-  background-color: #e3f2fd;
-  font-size: 24px;
-  font-family: "Microsoft JhengHei", Helvetica, Arial, sans-serif;
-  text-align: center;
-} */
 </style>
