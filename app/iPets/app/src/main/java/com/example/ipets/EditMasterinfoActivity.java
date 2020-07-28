e262cd32efe9d0bed00a1b6cdfed22f6f9e20ebe
lululiu29783638 <lululiu29783638@gmail.com>
2020-07-29 02:14:56 +0800

    modify EditMasterinfoActivity.java
    add sexSpinner.setAdapter

--- app/iPets/app/src/main/java/com/example/ipets/EditMasterinfoActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/EditMasterinfoActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class EditMasterinfoActivity extends AppCompatActivity {
+public class EditMasterinfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,21 @@ public class EditMasterinfoActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("編輯主人基本資料");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar3.setTitleTextColor(Color.WHITE);
+
+        Spinner sexSpinner = findViewById(R.id.sexSpinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.Spinner_sex,R.layout.myspinner_layout);
+        adapter.setDropDownViewResource(R.layout.myspinner_dropdown_layout);
+        sexSpinner.setAdapter(adapter);
+        sexSpinner.setOnItemSelectedListener(this  );
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
