60bb65aea2c679c993aef7246bc725263235a476
n1066423 <lululiu.29783638@gmail.com>
2020-07-30 16:18:48 +0800

    create EditAccountActivity.xml , .java
    add btn_editAcct

--- app/iPets/app/src/main/java/com/example/ipets/EditMasterinfoActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/EditMasterinfoActivity.java
@@ -3,6 +3,7 @@ package com.example.ipets;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,10 +11,13 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class EditMasterinfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    LinearLayout btn_editAcct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +31,22 @@ public class EditMasterinfoActivity extends AppCompatActivity implements Adapter
         toolbar3.setTitleTextColor(Color.WHITE);
 
         Spinner sexSpinner = findViewById(R.id.sexSpinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.Spinner_sex,R.layout.myspinner_layout);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Spinner_sex, R.layout.myspinner_layout);
         adapter.setDropDownViewResource(R.layout.myspinner_dropdown_layout);
         sexSpinner.setAdapter(adapter);
-        sexSpinner.setOnItemSelectedListener(this  );
+        sexSpinner.setOnItemSelectedListener(this);
+
+        btn_editAcct = (LinearLayout) findViewById(R.id.btnAccount);
+        btn_editAcct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentAcct = new Intent(EditMasterinfoActivity.this,EditAccountActivity.class);
+                startActivity(intentAcct);
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
