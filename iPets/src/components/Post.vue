2bbc40f1cdf83d4ce26e2d18ac67adec09f3f4a9
xintian <watson050308@gmail.com>
2020-07-31 12:48:08 +0800

    modify dogScience to watch router

--- iPets/src/components/Post.vue
+++ iPets/src/components/Post.vue
@@ -43,33 +43,26 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
-      title: "",
-      image: "",
-      introduction: "",
-      title1: "",
-      img1: "",
-      content1: "",
-      title2: "",
-      img2: "",
-      content2: "",
-      title3: "",
-      img3: "",
-      content3: "",
-      title4: "",
-      img4: "",
-      content4: "",
-      title5: "",
-      img5: "",
-      content5: "",
-      ending: "",
       comments: []
     };
   },
   mounted() {
+    let id = this.$route.params.id;
+    this.id = id;
     axios.get("http://localhost:3000/comments/" + this.id).then(res => {
       console.log(res.data);
       this.comments = res.data;
     });
+  },
+  watch: {
+    $route(now) {
+      let id = this.$route.params.id;
+      this.id = id;
+      axios.get("http://localhost:3000/comments/" + this.id).then(res => {
+        console.log(now);
+        this.comments = res.data;
+      });
+    }
   }
 };
 </script>
