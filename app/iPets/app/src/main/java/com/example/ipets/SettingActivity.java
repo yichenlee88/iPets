a42ec0b57074bd8ae4fe358ed5571770e457ef85
n1066414 <n1066414@ntub.edu.tw>
2020-08-03 01:18:01 +0800

    add signout function

--- app/iPets/app/src/main/java/com/example/ipets/SettingActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/SettingActivity.java
@@ -3,8 +3,14 @@ package com.example.ipets;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -18,5 +24,18 @@ public class SettingActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("設定");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar2.setTitleTextColor(Color.WHITE);
+        Button signout = findViewById(R.id.signout);
+        signout.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.signOut();
+                Intent intent=new Intent();
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setClass(SettingActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
