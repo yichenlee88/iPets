db0dbba5a4632844fe9987bdbd1dc8cdbede478a
xintian <watson050308@gmail.com>
2020-08-31 22:59:11 +0800

    modify manager route with article and user

---
+++ iPets/src/components/Manage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="Manage">
+      <b-container>
+        <b-row class="my-1" style="text-align:center">
+          <b-col cols="12">目前登入帳號：10646043</b-col>
+          <b-col cols="12">管理員名稱：蔡欣恬</b-col>
+          <b-col cols="12">管理權限：最高管理員</b-col>
+        </b-row>
+        <div class="center">
+          <router-link to="/Manage/manageArticle">管理文章</router-link>
+          <router-link to="/Manage/manageUser">管理使用者資訊</router-link>
+          <router-view></router-view>
+        </div>
+      </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Manage",
+  data() {}
+};
+</script>
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
---
+++ iPets/src/components/manageUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="manageUser">
+    <b-container style="margin-top:20px">
+      This is manageUser page
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "manageUser",
+  data() {}
+};
+</script>
+
+<style>
+</style>
