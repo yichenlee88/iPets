f78269e1da8cd8ff5b03744f52b6abab8621e480
N1066405 <n1066405@ntub.edu.tw>
2020-09-09 17:39:48 +0800

    modify EditContactusActivity.java；modify EditQiestionActivity.java

--- app/iPets/app/src/main/java/com/example/ipets/EditContactusActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/EditContactusActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class EditContactusActivity extends AppCompatActivity {
+public class EditContactusActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,29 @@ public class EditContactusActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("聯絡我們");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar8.setTitleTextColor(Color.WHITE);
+
+        Spinner problemSpinner = findViewById(R.id.problemSpinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Spinner_problem, R.layout.myspinner_layout);
+        adapter.setDropDownViewResource(R.layout.myspinner_dropdown_layout);
+        problemSpinner.setAdapter(adapter);
+        problemSpinner.setOnItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
