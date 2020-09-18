0983293dba819c41b26d1637b2e986135cf69f8a
xintian <watson050308@gmail.com>
2020-09-18 15:42:00 +0800

    modify dogInfo

--- iPets/src/components/DogInfo.vue
+++ iPets/src/components/DogInfo.vue
@@ -1,98 +1,33 @@
 <template>
   <!--https://www.hk01.com/%E5%AF%B5%E7%89%A9/202205/%E4%BA%86%E8%A7%A35%E5%80%8B%E7%8B%97%E7%8B%97%E5%B8%B8%E8%A6%8B%E5%BF%83%E7%90%86%E7%8B%80%E6%85%8B-%E8%AE%93%E4%BA%BA%E5%AF%B5%E9%97%9C%E4%BF%82%E6%9B%B4%E5%92%8C%E7%9D%A6-->
-  <div class="container col-8" style="margin-top: 20px;">
-    <h1 class="title">人與狗的關係</h1>
-    <div id="section1" class="row my-3" style="margin-top: 20px; background-color:#FFE4E1;">
-      <div class="col-lg-4">
-        <img src="../assets/人狗-01.jpg" width="300px" height="300px">
-      </div>
-      <div class="col-lg-8 columnberight">
-        <div class="media">
-          <div class="media-body">
-            <h3>霸佔地盤心理</h3>
-            <a href="https://anntw.com/articles/20180311-Kqxi"></a>
-            <p>
-              正是因為狗狗有強烈的地盤心態，所以才能勝任巡邏、保護主人財產的責任。而狗狗霸地盤，一般最常用的方法，就是排尿作氣味標記。因此，帶狗狗散步時，牠總是對著柱子留下氣味，想把自己的氣味留下來，告知其他狗狗自己的存在。另外，有些佔有慾極強的狗狗，就算家中只有兩隻狗狗，還是會霸佔自己的地盤，撒尿到另一隻狗的床上，以宣示主權。還有狗狗喜歡將東西藏起來，其實亦是佔有慾強，喜歡霸東西的表現。
-              <br>
-            </p>
-            <h5>改善方法：</h5>透過替狗狗絕育，能有效減低狗狗撒尿霸地盤的情況。另外，當狗狗在適當位置撒尿時，便給予獎勵，也能改善狗狗隨處撒尿霸地盤的情況。
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="section2" class="row my-3" style="margin-top: 20px; background-color:#E6E6FA;">
-      <div class="col-lg-8 columnberight">
-        <div class="media">
-          <div class="media-body">
-            <h3>依戀舊物心態</h3>
-            <a href="https://anntw.com/articles/20180311-Kqxi"></a>
-            <p>
-              大家都知道狗狗是很忠於主人，就算被遺棄，仍是對舊主人念念不忘。尤其是成年的狗狗，換了主人後會對陌生環境悶悶不樂，對新主人冷淡對待。如果你有領養過狗狗，就應該會有所體會。基本上來說，半歲到兩歲期間的狗狗，會產生強烈的依戀心理。常說，一隻狗狗一生只認一個主人，就是這個年齡階段。這時誰來照顧狗狗，牠就會視他為一生的同伴和主人。
-              <br>
-            </p>
-            <h5>改善方法：</h5>既然狗狗是如此忠誠，作為寵主要做的事很簡單，就是領養不棄養，不要讓狗狗失去主人。
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <img src="../assets/人狗-02.jpg" width="300px" height="300px">
-      </div>
-    </div>
-    <div id="section3" class="row my-3" style="margin-top: 20px; background-color:#F0FFD0;">
-      <div class="col-lg-4">
-        <img src="../assets/人狗-03.jpg" width="300px" height="300px">
-      </div>
-      <div class="col-lg-8 columnberight">
-        <div class="media">
-          <div class="media-body">
-            <h3>嫉妒吃醋心理</h3>
-            <a href="https://anntw.com/articles/20180311-Kqxi"></a>
-            <p>
-              當你家養了兩隻或以上的狗狗時，狗狗的嫉妒心便很容易看到。狗狗會順從主人，同時亦希望主人只愛牠一個。如果你對狗狗們厚此薄彼的話，很容易便會引起狗狗們之間的妒忌，從而出現排斥與爭鬥的情況。尤其是小型犬隻，牠們喜歡黏人，嫉妒心態也更明顯。
-              <br>
-            </p>
-            <h5>改善方法：</h5>如果家中如養了多隻狗狗，最好就是適當地分配時間予每隻狗狗，當狗狗都有與主人玩耍的時間，那麼狗狗便不會這麼容易吃醋。
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="section4" class="row my-3" style="margin-top: 20px; background-color:#FFEFD5;">
-      <div class="col-lg-8 columnberight">
-        <div class="media">
-          <div class="media-body">
-            <h3>記仇報復心理</h3>
-            <a href="https://anntw.com/articles/20180311-Kqxi"></a>
-            <p>
-              對於不喜歡狗狗的人來說，狗許多時都「狗仗欺人」，藉著自己的優勢來欺負弱小，而且復仇心很重。其實這心態不難理解，人類都尚且如此，何況是狗狗。對於曾對自己不好的事，會牢記在心中，這記憶很難被抹除。由於狗狗不會說話，我們沒法得知狗狗為何會不高興。因此，如果你發現狗狗對某東西或某人懷有敵意，反應特別大的話，寵主應儘量避免讓狗狗再去接觸。
-              <br>
-            </p>
-            <h5>改善方法：</h5>有研究指狗狗的復仇心理，可能是因有著土狼的遺傳基因所致，因此難以令狗狗作出改變。最好的方法，就是別惹怒狗狗，以及避免讓狗狗與衝突者碰上。
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <img src="../assets/人狗-04.jpg" width="300px" height="300px">
-      </div>
-    </div>
-    <div id="section5" class="row my-3" style="margin-top: 20px; background-color:#F0DFF0;">
-      <div class="col-lg-4">
-        <img src="../assets/人狗-05.jpg" width="300px" height="300px">
-      </div>
-      <div class="col-lg-8 columnberight">
-        <div class="media">
-          <div class="media-body">
-            <h3>階級高低心理</h3>
-            <a href="https://anntw.com/articles/20180311-Kqxi"></a>
-            <p>
-              在眾多寵物當中，相信就只有狗狗是最會聽從指令，順從於主人意思。基本上來說，當你領養狗狗後，就要讓狗狗知道人狗之間的地位，主人是狗狗的領導者，是「老大」。但如果過於溺愛狗狗，主人在狗狗心裡的地位可能就會動搖產生變化。當狗狗覺得主人地位低於自己時，狗狗就會不再順從主人的指令，甚至會恐嚇攻擊主人。
-              <br>
-            </p>
-            <h5>改善方法：</h5>要重新建立起正確的主從關係，讓狗狗知道誰才是主人，因此要建立起一定的權威。例如不要經常抱狗狗、抱狗狗時不要超過自己的頭位置、出門或回家時都不要太理睬狗狗，待牠冷靜下來才摸牠。讓狗狗明白到，牠不能主導一切。
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <b-container>
+    <h1 class="title top">人與狗的關係</h1>
+    <img
+      class="d-block img-fluid top"
+      src="../assets/dogInfo-01.png"
+      alt="image slot"
+    >
+    <img
+      class="d-block img-fluid top"
+      src="../assets/dogInfo-02.png"
+      alt="image slot"
+    >
+    <img
+      class="d-block img-fluid top"
+      src="../assets/dogInfo-03.png"
+      alt="image slot"
+    >
+    <img
+      class="d-block img-fluid top"
+      src="../assets/dogInfo-04.png"
+      alt="image slot"
+    >
+    <img
+      class="d-block img-fluid top"
+      src="../assets/dogInfo-05.png"
+      alt="image slot"
+    >
+  </b-container>
 </template>
 
 <script>
@@ -102,16 +37,11 @@ export default {
 </script>
 
 <style scoped>
-.columnberight {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.top {
+  margin-top: 20px;
 }
 .title {
   padding: 10px;
-  background: rgba(255, 246, 212, 0.71);
-  color: #8f7061;
-  border-radius: 25px;
   text-align: center;
   margin: auto;
   max-width: 300px;
