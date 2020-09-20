3f2728440411d65d82fb06ca57d4ef5cb986a178
n1066414 <n1066414@ntub.edu.tw>
2020-09-21 02:13:10 +0800

    create AddNoteActivity,NotePreviewActivity

---
+++ app/iPets/app/src/main/java/com/example/ipets/AddNoteActivity.java
@@ -0,0 +1,20 @@
+package com.example.ipets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+public class AddNoteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+    }
+}
---
+++ app/iPets/app/src/main/java/com/example/ipets/NotePreviewActivity.java
@@ -0,0 +1,15 @@
+package com.example.ipets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotePreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_preview);
+
+    }
+}
