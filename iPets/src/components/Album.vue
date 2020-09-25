526b5010b43ace09c6dfa385d1ae521c0a691ff5
YichenLee <10646004@ntub.edu.tw>
2020-09-25 16:20:43 +0800

    add not album page

--- iPets/src/components/Album.vue
+++ iPets/src/components/Album.vue
@@ -1,43 +1,19 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex class="text-center font-weight-black">
-        <h1>Upload a photo</h1>
+    <v-layout class="border-bottom" row>
+      <v-flex class="high text-left font-weight-black">
+        <h1>相簿</h1>
       </v-flex>
-    </v-layout>
-
-    <v-layout row>
-      <v-flex md6 offset-sm3>
-        <div>
-          <div>
-            <v-btn @click="click1">choose photo</v-btn>
-            <input
-              type="file"
-              ref="input1"
-              style="display: none"
-              @change="previewImage"
-              accept="image/*"
-            />
-          </div>
-
-          <div v-if="imageData != null">
-            <img class="preview" height="268" width="356" :src="img1" />
-            <br />
-          </div>
-        </div>
-      </v-flex>
-    </v-layout>
-
-    <v-layout row>
-      <v-flex md6 offset-sm3 class="text-center">
-        <v-text-field solo v-model="caption" label="Caption goes here">
-        </v-text-field>
-      </v-flex>
+      <v-btn class="btn-right" style="margin-top:30px" @click="click1"
+        >＋新增相簿</v-btn
+      >
     </v-layout>
     <v-layout row>
-      <v-flex class="text-center">
-        <v-btn color="pink" @click="create">upload</v-btn>
-      </v-flex>
+      <b-img
+        class="banner_png center"
+        src="../static/img/gray.png"
+        style="width:468px;"
+      ></b-img>
     </v-layout>
   </v-container>
 </template>
@@ -106,3 +82,21 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.high {
+  margin-top: 20px;
+}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+}
+
+.border-bottom {
+  height: 90px;
+  border-bottom: 2px solid #888888 !important;
+}
+</style>
