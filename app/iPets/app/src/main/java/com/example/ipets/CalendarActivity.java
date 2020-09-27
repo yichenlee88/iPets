38d102b5fe07423770de240d837f0d83242d6e52
n1066414 <n1066414@ntub.edu.tw>
2020-09-27 17:57:22 +0800

    add present event  in calendar function

--- app/iPets/app/src/main/java/com/example/ipets/CalendarActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/CalendarActivity.java
@@ -1,39 +1,43 @@
 package com.example.ipets;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
-import com.applandeo.materialcalendarview.exceptions.OutOfDateRangeException;
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class CalendarActivity extends AppCompatActivity {
-
-        public static final String RESULT = "result";
-        public static final String EVENT = "event";
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = auth.getCurrentUser();
+    String userUID = currentUser.getUid();
         private static final int ADD_NOTE = 44;
-
         private CalendarView mCalendarView;
-        private List<EventDay> mEventDays = new ArrayList<>();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_calendar);
-
-            mCalendarView = (CalendarView) findViewById(R.id.calendarView);
-
+            mCalendarView = findViewById(R.id.calendarView);
             FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.Add);
             floatingActionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -48,21 +52,7 @@ public class CalendarActivity extends AppCompatActivity {
                     previewNote(eventDay);
                 }
             });
-        }
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            if (requestCode == ADD_NOTE && resultCode == RESULT_OK) {
-                MyEventDay myEventDay = data.getParcelableExtra(RESULT);
-                try {
-                    mCalendarView.setDate(myEventDay.getCalendar());
-                } catch (OutOfDateRangeException e) {
-                    e.printStackTrace();
-                }
-                mEventDays.add(myEventDay);
-                mCalendarView.setEvents(mEventDays);
-            }
         }
 
         private void addNote() {
@@ -72,9 +62,38 @@ public class CalendarActivity extends AppCompatActivity {
 
         private void previewNote(EventDay eventDay) {
             Intent intent = new Intent(this, NotePreviewActivity.class);
-            if(eventDay instanceof MyEventDay){
-                intent.putExtra(EVENT, (MyEventDay) eventDay);
-            }
             startActivity(intent);
         }
+        protected void onStart() {
+            super.onStart();
+
+            mCalendarView = findViewById(R.id.calendarView);
+            final FirebaseFirestore db = FirebaseFirestore.getInstance();
+            List events = new ArrayList<>();
+            db.collection("userInformation").document(userUID).collection("calendar").get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String date = document.getId();
+                                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                                    try {
+                                        Date calenderdate = sdf.parse(date);
+                                        Calendar cal = Calendar.getInstance();
+                                        cal.setTime(calenderdate);
+                                        events.add(new EventDay(cal, R.drawable.ic_home));
+                                        mCalendarView.setEvents(events);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            } else {
+
+                            }
+                        }
+                    });
+
+        }
+
     }
