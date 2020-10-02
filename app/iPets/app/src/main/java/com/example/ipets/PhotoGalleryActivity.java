a12c9b797c16080772825bd7fe73d78d37ff23ed
lululiu29783638 <lululiu29783638@gmail.com>
2020-10-02 18:30:35 +0800

    add Toolbar function in PhotoGalleryActivity.java

--- app/iPets/app/src/main/java/com/example/ipets/PhotoGalleryActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/PhotoGalleryActivity.java
@@ -1,7 +1,9 @@
 package com.example.ipets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class PhotoGalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_gallery);
+
+        Toolbar toolbar9 = findViewById(R.id.toolbar9);
+        setSupportActionBar(toolbar9);
+        getSupportActionBar().setTitle("我的寵物");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar9.setTitleTextColor(Color.WHITE);
     }
 }
