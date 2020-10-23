dee73347e6d4c0785e7ffb459b4a0c51fe019ad3
YichenLee <10646004@ntub.edu.tw>
2020-10-24 00:36:44 +0800

    modify Navbar and resolve can't expand problem

--- iPets/src/components/Navbar.vue
+++ iPets/src/components/Navbar.vue
@@ -1,56 +1,49 @@
 <template>
-  <b-navbar toggleable="lg" variant="faded" type="light" class="navcolor">
-    <b-navbar-brand href="/" style="  font-size: 24px;">iPets</b-navbar-brand>
-    <!-- Right aligned nav items -->
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-navbar-nav class="ml-auto">
+  <div>
+    <b-navbar class="navbar-custom" toggleable="md">
+      <b-navbar-brand href="/" style="font-size: 30px;">iPets</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
       <b-collapse id="nav-collapse" is-nav>
-        <b-nav-item href="#/about" v-if="!isSignedIn">關於我們</b-nav-item>
-        <b-nav-item href="#/dogScience" v-if="!isSignedIn">狗狗科普</b-nav-item>
-        <b-nav-item href="#/dogInfo" v-if="!isSignedIn"
-          >人與狗的關係</b-nav-item
-        >
-        <b-nav-item href="#/contact" v-if="!isSignedIn">聯繫我們</b-nav-item>
-        <b-nav-item href="#/calendar" v-if="isSignedIn">行事曆</b-nav-item>
-        <b-nav-item href="#/album" v-if="isSignedIn">相簿</b-nav-item>
-        <b-nav-item href="#/member" v-if="isSignedIn">會員專區</b-nav-item>
-        <b-nav-item href="#/setting/EditProfile" v-if="isSignedIn"
-          >設定</b-nav-item
-        >
-        <b-nav-form>
-          <b-nav-item @click="logout" v-if="isSignedIn"
-            ><i class="fas fa-sign-in-alt" style="size:12px"></i
-            >登出</b-nav-item
+        <b-navbar-nav>
+          <b-nav-item href="#/about" v-if="!isSignedIn">關於我們</b-nav-item>
+          <b-nav-item href="#/dogScience" v-if="!isSignedIn"
+            >狗狗科普</b-nav-item
           >
-        </b-nav-form>
-        <b-nav-item href="#/register" v-if="isSignedIn"
-          ><i class="far fa-user" style="size:12px"></i
-        ></b-nav-item>
-        <b-nav-form>
-          <b-nav-item href="#/login" v-if="!isSignedIn"
-            ><i class="fas fa-sign-in-alt" style="size:12px"></i
-            >登入</b-nav-item
+          <b-nav-item href="#/dogInfo" v-if="!isSignedIn"
+            >人與狗的關係</b-nav-item
           >
-          <b-nav-item href="#/register" v-if="!isSignedIn"
-            ><i class="far fa-user" style="size:12px"></i>註冊</b-nav-item
+          <b-nav-item href="#/contact" v-if="!isSignedIn">聯繫我們</b-nav-item>
+          <b-nav-item href="#/calendar" v-if="isSignedIn">行事曆</b-nav-item>
+          <b-nav-item href="#/album" v-if="isSignedIn">相簿</b-nav-item>
+          <b-nav-item href="#/member" v-if="isSignedIn">會員專區</b-nav-item>
+          <b-nav-item href="#/setting/EditProfile" v-if="isSignedIn"
+            >設定</b-nav-item
           >
-          <b-form-input
-            class="mr-sm-2"
-            placeholder="Search"
-            v-if="!isSignedIn"
-            style="margin-top:8px"
-          ></b-form-input>
-          <b-button
-            class="my-2"
-            type="submit"
-            v-if="!isSignedIn"
-            style="margin-top:10px"
-            >Search</b-button
-          >
-        </b-nav-form>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-nav-item href="#/login" v-if="!isSignedIn"
+              ><i class="fas fa-sign-in-alt" style="size:12px"></i
+              >登入</b-nav-item
+            >
+            <b-nav-item href="#/register" v-if="!isSignedIn"
+              ><i class="far fa-user" style="size:12px"></i>註冊</b-nav-item
+            >
+          </b-nav-form>
+          <b-nav-item href="#/" v-if="!isSignedIn">網站導覽</b-nav-item>
+          <b-nav-form>
+            <b-nav-item @click="logout" v-if="isSignedIn"
+              ><i class="fas fa-sign-in-alt" style="size:12px"></i
+              >登出</b-nav-item
+            >
+          </b-nav-form>
+        </b-navbar-nav>
       </b-collapse>
-    </b-navbar-nav>
-  </b-navbar>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -85,17 +78,17 @@ export default {
 };
 </script>
 <style>
-.navcolor {
-  background-color: white;
-}
+/* .navbar-custom {
+  background-color: #d2e9ff;
+} */
+
 .navbar-brand,
 .form-input,
 .botton,
 .nav-item {
-  height: 22px;
-  width: 150px;
-  font-size: 18px;
-  text-align: center;
+  height: auto;
+  width: auto;
+  font-size: 22px;
 }
 
 @media screen and (max-width: 768px) {
