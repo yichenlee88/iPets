ae354e25c0e898d33a00e740a13b1c17b75205ea
n1066414 <n1066414@ntub.edu.tw>
2020-10-28 20:48:18 +0800

    add edit pet info function

--- app/iPets/app/src/main/java/com/example/ipets/EditPetsinfoActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/EditPetsinfoActivity.java
@@ -1,20 +1,64 @@
 package com.example.ipets;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditPetsinfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = auth.getCurrentUser();
+    String userUID = currentUser.getUid();
+    private final static int CAMERA = 1;
+    private final static int PHOTO = 2;
+    private DisplayMetrics mPhone;
+    private static final String FILE_PATH = "/sdcard/ipets/jpeg";
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +75,235 @@ public class EditPetsinfoActivity extends AppCompatActivity implements AdapterVi
         adapter.setDropDownViewResource(R.layout.myspinner_dropdown_layout);
         petSexSpinner.setAdapter(adapter);
         petSexSpinner.setOnItemSelectedListener(this);
+        setEditText();
+        toolbar6.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.check:
+                        addImage();
+                        break;
+                }
+                return false;
+            }
+        });
+        ImageButton album = findViewById(R.id.album);
+        album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, PHOTO);
+            }
+        });
+        ImageButton camera1 = findViewById(R.id.camera1);
+        camera1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, CAMERA);
+            }
+        });
+        getPetInfo();
+        getPetImage();
+    }
+    public void getPetImage(){
+        Intent intent = this.getIntent();
+        String pet = intent.getStringExtra("date");
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://ipets-app.appspot.com");
+        StorageReference mStorageRef = storage.getReference();
+        StorageReference islandRef = mStorageRef.child(userUID +'/'+ pet+".jpg");
+        img = findViewById(R.id.petsHead);
+        final long ONE_MEGABYTE = 3000 * 3000;
 
+        islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Glide.with(getApplicationContext())
+                        .load(bytes)
+                        .into(img);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors'
+            }
+        });
+    }
+
+    private void getPetInfo() {
+        EditText edmypetName = findViewById(R.id.mypetName);
+        final EditText edpetsbirth = findViewById(R.id.mypetBirth);
+        String[] petSex =getResources().getStringArray(R.array.Spinner_petSex);
+        Spinner petSexSpinner = findViewById(R.id.petSexSpinner);
+        Intent intent = this.getIntent();
+        //取得傳遞過來的資料
+        String pet = intent.getStringExtra("date");
+        FirebaseFirestore db;
+        db = FirebaseFirestore.getInstance();
+        db.collection("userInformation").document(userUID).collection("pets").document(pet)
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    StringBuilder fields = new StringBuilder("");
+                    StringBuilder fields2 = new StringBuilder("");
+                    StringBuilder fields3 = new StringBuilder("");
+                    fields.append(doc.get("Petsname")).toString();
+                    String Petsgender = fields2.append(doc.get("Petsgender")).toString();
+                    fields3.append(doc.get("Petsbirth")).toString();
+                    edmypetName.setText(fields);
+                    edpetsbirth.setText(fields3);
+                    if(Petsgender.equals("母的")) {
+                        petSexSpinner.setSelection(1);
+                    }
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
+    }
+
+    private void addImage() {
+        EditText edmypetName = findViewById(R.id.mypetName);
+        final String mypetName = edmypetName.getText().toString();
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://ipets-app.appspot.com");
+        StorageReference mStorageRef = storage.getReference();
+        StorageReference mountainsRef = mStorageRef.child(userUID +'/'+ mypetName+".jpg");
+
+        Bitmap bitmap = ((BitmapDrawable) img.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        UploadTask uploadTask = mountainsRef.putBytes(data);
+        //取得照片網址
+        String petsimage = String.valueOf(mountainsRef.getDownloadUrl());
+        addPetInfo(petsimage,mypetName);
+    }
+
+    private void addPetInfo(String petsimage, String mypetName) {
+        final EditText edpetsbirth = findViewById(R.id.mypetBirth);
+        String petsbirth = edpetsbirth.getText().toString();
+        String[] petSex =getResources().getStringArray(R.array.Spinner_petSex);
+        Spinner petSexSpinner = findViewById(R.id.petSexSpinner);
+        int idsex=petSexSpinner.getSelectedItemPosition();
+        String sex = petSex[idsex];
+        FirebaseFirestore db;
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("Petsimage", petsimage);
+        userInfo.put("Petsname", mypetName);
+        userInfo.put("Petsbirth", petsbirth);
+        userInfo.put("Petsgender", sex);
+        db.collection("userInformation").document(userUID).collection("pets").document(mypetName).set(userInfo);
+        AlertDialog.Builder finishsignup = new AlertDialog.Builder(EditPetsinfoActivity.this);
+        finishsignup.setMessage("修改成功");
+        finishsignup.setNegativeButton("確認", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                Intent intent=new Intent();
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setClass(EditPetsinfoActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        finishsignup.setCancelable(false);
+        finishsignup.show();
+    }
+    public void setEditText(){
+        EditText edmypetBirth = findViewById(R.id.mypetBirth);
+        edmypetBirth.setInputType(InputType.TYPE_NULL); //不顯示系統輸入鍵盤
+        edmypetBirth.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                // TODO Auto-generated method stub
+                if(hasFocus){
+                    Calendar c = Calendar.getInstance();
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(v.getContext(),R.style.MyDatePickerDialogTheme, new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            // TODO Auto-generated method stub
+                            edmypetBirth.setText(year+"/"+(monthOfYear+1)+"/"+dayOfMonth);
+                        }
+                    }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+                    DatePicker datePicker = datePickerDialog.getDatePicker();
+                    datePicker.setMaxDate(System.currentTimeMillis());
+                    datePickerDialog.show();
+
+                }
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //讀取手機解析度
+        mPhone = new DisplayMetrics();
+        EditPetsinfoActivity.this.getWindowManager().getDefaultDisplay().getMetrics(mPhone);
+        if (requestCode == PHOTO && data != null) {
+            //取得照片路徑uri
+            Uri uri = data.getData();
+            ContentResolver cr = EditPetsinfoActivity.this.getContentResolver();
+            try {
+                //讀取照片，型態為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
+                if (bitmap.getWidth() > bitmap.getHeight()) scaleImage(bitmap,
+                        mPhone.heightPixels);
+                else scaleImage(bitmap, mPhone.widthPixels);
+            } catch (FileNotFoundException e) {
+            }
+        }
+        if (requestCode == CAMERA && data != null) {
+            Bitmap photo = null;
+            if (data.getData() != null || data.getExtras() != null) { //防止沒有返回結果
+                Uri camerauri = data.getData();
+                if (camerauri != null) {
+                    photo = BitmapFactory.decodeFile(camerauri.getPath()); //拿到圖片
+                }
+                if (photo == null) {
+                    Bundle bundle = data.getExtras();
+                    if (bundle != null) {
+                        photo = (Bitmap) bundle.get("data");
+                    }
+                }
+                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
+                if (photo.getWidth() > photo.getHeight()) scaleImage(photo,
+                        mPhone.heightPixels);
+                else scaleImage(photo, mPhone.widthPixels);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void scaleImage(Bitmap bitmap, int phone) {
+        //縮放比例預設為1
+        float mScale = 1;
+        img = findViewById(R.id.petsHead);
+        //如果圖片寬度大於手機寬度則進行縮放，否則直接將圖片放入ImageView內
+        if (bitmap.getWidth() > phone) {
+            //判斷縮放比例
+            mScale = (float) phone / (float) bitmap.getWidth();
+
+            Matrix mMat = new Matrix();
+            mMat.setScale(mScale, mScale);
+
+            Bitmap mScaleBitmap = Bitmap.createBitmap(bitmap,
+                    0,
+                    0,
+                    bitmap.getWidth(),
+                    bitmap.getHeight(),
+                    mMat,
+                    false);
+            img.setImageBitmap(mScaleBitmap);
+        } else img.setImageBitmap(bitmap);
     }
 
     @Override
@@ -43,7 +315,7 @@ public class EditPetsinfoActivity extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
