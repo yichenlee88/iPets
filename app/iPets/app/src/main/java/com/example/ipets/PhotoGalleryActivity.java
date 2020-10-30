38e14167945de4eda4af2b92f88ebfb8fcf6a9f1
N1066405 <n1066405@ntub.edu.tw>
2020-10-30 17:06:10 +0800

    add PhotoGalleryActivity.java
    chose multiple photo from app gallrey

--- app/iPets/app/src/main/java/com/example/ipets/PhotoGalleryActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/PhotoGalleryActivity.java
@@ -1,59 +1,126 @@
 package com.example.ipets;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.GridView;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
-    private GridView photoView;
-    private final static int ADDPHOTO = 1;
-    private static final String FILE_PATH = "/sdcard/ipets/jpeg";
-    FirebaseAuth auth = FirebaseAuth.getInstance();
-    FirebaseUser currentUser = auth.getCurrentUser();
-    String userUID = currentUser.getUid();
-    private GridView mGridView;
+
+    public static Uri selectedImage;
+
+    //上傳按鈕
+    Button bUploadImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_gallery);
-        mGridView = findViewById(R.id.photoView);
-        FloatingActionButton addPhoto = (FloatingActionButton) findViewById(R.id.addPhoto);
-        addPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addPhotoIntent = new Intent();
-                addPhotoIntent.setType("image/*");
-                addPhotoIntent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(addPhotoIntent,ADDPHOTO);
-            }
-        });
-        /*mGridView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadimage();
-            }
-        });*/
-
-
 
         Toolbar toolbar9 = findViewById(R.id.toolbar9);
         setSupportActionBar(toolbar9);
         getSupportActionBar().setTitle("我的寵物");
         toolbar9.setTitleTextColor(Color.WHITE);
+
+        init();
+
     }
 
-    /*private void uploadimage() {
+    // 初始化，給一些對象賦值
+    private void init() {
 
+        bUploadImage = (Button)findViewById(R.id.bt_select_image);
+        bUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(PhotoGalleryActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
 
-    }*/
+                    ActivityCompat.requestPermissions(PhotoGalleryActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            100);
+
+                    return;
+                }
+                Intent addPhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                addPhotoIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+                addPhotoIntent.setType("image/*");
+                startActivityForResult(addPhotoIntent,1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            ImageView imageView = findViewById(R.id.uploadedImage);
+            //ImageView imageView2 = findViewById(R.id.uploadedImage2);
+            List<Bitmap> bitmaps = new ArrayList<>();
+            ClipData clipData = data.getClipData();
+            if (clipData != null) {
+                for (int i = 0; i < clipData.getItemCount(); i++) {
+                    Uri imageUri = clipData.getItemAt(i).getUri();
+                    try {
+                        InputStream is = getContentResolver().openInputStream(imageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        bitmaps.add(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                Uri imageUri = data.getData();
+                try {
+                    InputStream is = getContentResolver().openInputStream(imageUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    bitmaps.add(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (final Bitmap b : bitmaps) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(b);
+                                //imageView2.setImageBitmap(b);
+                            }
+                        });
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
 }
+
+
