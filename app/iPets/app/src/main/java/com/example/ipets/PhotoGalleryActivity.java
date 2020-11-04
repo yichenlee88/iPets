18799a9d4a2636417778b9a0237ee7de3a9cd397
N1066405 <n1066405@ntub.edu.tw>
2020-11-04 17:57:16 +0800

    modify PhotoGalleryActivity.java
    gridview displays thumbnails in the photo album

--- app/iPets/app/src/main/java/com/example/ipets/PhotoGalleryActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/PhotoGalleryActivity.java
@@ -1,34 +1,46 @@
 package com.example.ipets;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.ClipData;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
 
-    public static Uri selectedImage;
+    private GridView gridView;
+    private ImageView imageView;
+    FloatingActionButton bUploadImage; //上傳按鈕
+    private List<String> thumbs;  //存放縮圖的id
+    private List<String> imagePaths;  //存放圖片的路徑
+    private ImageAdapter imageAdapter;  //用來顯示縮圖
 
-    //上傳按鈕
-    Button bUploadImage;
+    private static final int PICK_IMAGE = 100;
+
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = auth.getCurrentUser();
+    String userUID = currentUser.getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +52,13 @@ public class PhotoGalleryActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("我的寵物");
         toolbar9.setTitleTextColor(Color.WHITE);
 
-        init();
-
-    }
-
-    // 初始化，給一些對象賦值
-    private void init() {
-
-        bUploadImage = (Button)findViewById(R.id.bt_select_image);
+        bUploadImage = findViewById(R.id.addPhoto);
         bUploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(PhotoGalleryActivity.this,
                         Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED){
+                        != PackageManager.PERMISSION_GRANTED) {
 
                     ActivityCompat.requestPermissions(PhotoGalleryActivity.this,
                             new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
@@ -61,66 +66,69 @@ public class PhotoGalleryActivity extends AppCompatActivity {
 
                     return;
                 }
-                Intent addPhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                addPhotoIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
-                addPhotoIntent.setType("image/*");
-                startActivityForResult(addPhotoIntent,1);
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                gallery.setType("image/*");
+                gallery.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(gallery, PICK_IMAGE);
             }
         });
 
-    }
+        gridView = findViewById(R.id.photoView);
+        imageView = findViewById(R.id.imageView1);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            ImageView imageView = findViewById(R.id.uploadedImage);
-            //ImageView imageView2 = findViewById(R.id.uploadedImage2);
-            List<Bitmap> bitmaps = new ArrayList<>();
-            ClipData clipData = data.getClipData();
-            if (clipData != null) {
-                for (int i = 0; i < clipData.getItemCount(); i++) {
-                    Uri imageUri = clipData.getItemAt(i).getUri();
-                    try {
-                        InputStream is = getContentResolver().openInputStream(imageUri);
-                        Bitmap bitmap = BitmapFactory.decodeStream(is);
-                        bitmaps.add(bitmap);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                Uri imageUri = data.getData();
-                try {
-                    InputStream is = getContentResolver().openInputStream(imageUri);
-                    Bitmap bitmap = BitmapFactory.decodeStream(is);
-                    bitmaps.add(bitmap);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+        ContentResolver cr = getContentResolver();
+        String[] projection = {MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA};
+
+        //查詢SD卡的圖片
+        Cursor cursor = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+
+        thumbs = new ArrayList<String>();
+        imagePaths = new ArrayList<String>();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+
+            cursor.moveToPosition(i);
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.Media._ID));// ID
+            thumbs.add(id + "");
+
+            String filepath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));//抓路徑
+
+            imagePaths.add(filepath);
+        }
+
+        cursor.close();
+
+        imageAdapter = new ImageAdapter(PhotoGalleryActivity.this, thumbs);
+        gridView.setAdapter(imageAdapter);
+        imageAdapter.notifyDataSetChanged();
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                imageView.setVisibility(View.GONE);
+                gridView.setVisibility(View.VISIBLE);
             }
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for (final Bitmap b : bitmaps) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                imageView.setImageBitmap(b);
-                                //imageView2.setImageBitmap(b);
-                            }
-                        });
-                        try {
-                            Thread.sleep(3000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }).start();
-        }
+        });
+        imageView.setVisibility(View.GONE);
+
     }
+
+    public void setImageView(int position) {
+        Bitmap bm = BitmapFactory.decodeFile(imagePaths.get(position));
+        imageView.setImageBitmap(bm);
+        imageView.setVisibility(View.VISIBLE);
+        gridView.setVisibility(View.GONE);
+    }
+
+
+
 }
+
+
+
+
 
 
