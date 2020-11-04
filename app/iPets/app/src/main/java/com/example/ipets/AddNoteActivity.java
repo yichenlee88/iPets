1e699e44c42c14423ac131d4f0c6bcfaa6e1a416
n1066414 <n1066414@ntub.edu.tw>
2020-11-04 17:59:24 +0800

    modify AddNoteActivity.java

--- app/iPets/app/src/main/java/com/example/ipets/AddNoteActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/AddNoteActivity.java
@@ -22,13 +22,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AddNoteActivity extends AppCompatActivity {
-    int startyear, startmonthOfYear, startdayOfMonth;
-    int endyear, endmonthOfYear, enddayOfMonth;
+Calendar endCalendar,startCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,9 +58,16 @@ public class AddNoteActivity extends AppCompatActivity {
                         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                             // TODO Auto-generated method stub
                             startDate.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
-                            startyear = year;
-                            startmonthOfYear = monthOfYear;
-                            startdayOfMonth = dayOfMonth;
+                            String startstr = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                            SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
+                            Date date = null;
+                            try {
+                                date = sdf.parse(startstr);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                            startCalendar = Calendar.getInstance();
+                            startCalendar.setTime(date);
                         }
                     }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
                     DatePicker datePicker = datePickerDialog.getDatePicker();
@@ -79,9 +88,16 @@ public class AddNoteActivity extends AppCompatActivity {
                         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                             // TODO Auto-generated method stub
                             endDate.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
-                            endyear = year;
-                            endmonthOfYear = monthOfYear;
-                            enddayOfMonth = dayOfMonth;
+                            String endstr = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                            SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
+                            Date date = null;
+                            try {
+                                date = sdf.parse(endstr);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                            endCalendar = Calendar.getInstance();
+                            endCalendar.setTime(date);
                         }
                     }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
                     DatePicker datePicker = datePickerDialog.getDatePicker();
@@ -133,19 +149,128 @@ public class AddNoteActivity extends AppCompatActivity {
         int idsex = repeatSpinner.getSelectedItemPosition();
         String[] Spinner_repeat = getResources().getStringArray(R.array.Spinner_repeat);
         String repeat = Spinner_repeat[idsex];
-        startmonthOfYear = startmonthOfYear+1;
-        endmonthOfYear = endmonthOfYear+1;
-        startdayOfMonth = startdayOfMonth -1;
-        enddayOfMonth = enddayOfMonth - 1;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String startday = sdf.format(startCalendar.getTime());
+        String endday = sdf.format(endCalendar.getTime());
+        FirebaseFirestore db;
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("eventName", note);
+        userInfo.put("startDate", startday);
+        userInfo.put("endDate", endday);
+        userInfo.put("frequency", repeat);
+        userInfo.put("details", detail);
+        userInfo.put("color", colorselect);
+        db.collection("users").document(userUID).collection("calEvent").document().set(userInfo);
+        if(repeat.equals("不重複")){
+            AlertDialog.Builder finishsignup = new AlertDialog.Builder(AddNoteActivity.this);
+            finishsignup.setMessage("新增成功");
+            finishsignup.setNegativeButton("確認", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    Intent intent = new Intent();
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setClass(AddNoteActivity.this, CalendarActivity.class);
+                    startActivity(intent);
+                }
+            });
+            finishsignup.setCancelable(false);
+            finishsignup.show();
+        }
         if (repeat.equals("每日")) {
-            for (int i = 0; i < 3; i++) {
-                startdayOfMonth = startdayOfMonth + 1;
-                String start = startyear + "-" + startmonthOfYear + "-" + startdayOfMonth;
-                enddayOfMonth = enddayOfMonth + 1;
-                String end = endyear + "-" + endmonthOfYear + "-" + enddayOfMonth;
-                FirebaseFirestore db;
+            for (int i = 0; i < 4; i++) {
+                startCalendar.add(Calendar.DAY_OF_MONTH,1);
+                endCalendar.add(Calendar.DAY_OF_MONTH,1);
+                String start = sdf.format(startCalendar.getTime());
+                String end = sdf.format(endCalendar.getTime());
                 db = FirebaseFirestore.getInstance();
-                Map<String, Object> userInfo = new HashMap<>();
+                userInfo.put("eventName", note);
+                userInfo.put("startDate", start);
+                userInfo.put("endDate", end);
+                userInfo.put("frequency", repeat);
+                userInfo.put("details", detail);
+                userInfo.put("color", colorselect);
+                db.collection("users").document(userUID).collection("calEvent").document().set(userInfo);
+            }
+            AlertDialog.Builder finishsignup = new AlertDialog.Builder(AddNoteActivity.this);
+            finishsignup.setMessage("新增成功");
+            finishsignup.setNegativeButton("確認", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    Intent intent = new Intent();
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setClass(AddNoteActivity.this, CalendarActivity.class);
+                    startActivity(intent);
+                }
+            });
+            finishsignup.setCancelable(false);
+            finishsignup.show();
+        }
+        if (repeat.equals("每週")) {
+            for (int i = 0; i < 4; i++) {
+                startCalendar.add(Calendar.DAY_OF_MONTH,7);
+                endCalendar.add(Calendar.DAY_OF_MONTH,7);
+                String start = sdf.format(startCalendar.getTime());
+                String end = sdf.format(endCalendar.getTime());
+                db = FirebaseFirestore.getInstance();
+                userInfo.put("eventName", note);
+                userInfo.put("startDate", start);
+                userInfo.put("endDate", end);
+                userInfo.put("frequency", repeat);
+                userInfo.put("details", detail);
+                userInfo.put("color", colorselect);
+                db.collection("users").document(userUID).collection("calEvent").document().set(userInfo);
+            }
+            AlertDialog.Builder finishsignup = new AlertDialog.Builder(AddNoteActivity.this);
+            finishsignup.setMessage("新增成功");
+            finishsignup.setNegativeButton("確認", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    Intent intent = new Intent();
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setClass(AddNoteActivity.this, CalendarActivity.class);
+                    startActivity(intent);
+                }
+            });
+            finishsignup.setCancelable(false);
+            finishsignup.show();
+        }
+        if (repeat.equals("每月")) {
+            for (int i = 0; i < 4; i++) {
+                startCalendar.add(Calendar.MONTH,1);
+                endCalendar.add(Calendar.MONTH,1);
+                String start = sdf.format(startCalendar.getTime());
+                String end = sdf.format(endCalendar.getTime());
+                db = FirebaseFirestore.getInstance();
+                userInfo.put("eventName", note);
+                userInfo.put("startDate", start);
+                userInfo.put("endDate", end);
+                userInfo.put("frequency", repeat);
+                userInfo.put("details", detail);
+                userInfo.put("color", colorselect);
+                db.collection("users").document(userUID).collection("calEvent").document().set(userInfo);
+            }
+            AlertDialog.Builder finishsignup = new AlertDialog.Builder(AddNoteActivity.this);
+            finishsignup.setMessage("新增成功");
+            finishsignup.setNegativeButton("確認", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    Intent intent = new Intent();
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setClass(AddNoteActivity.this, CalendarActivity.class);
+                    startActivity(intent);
+                }
+            });
+            finishsignup.setCancelable(false);
+            finishsignup.show();
+        }
+        if (repeat.equals("每年")) {
+            for (int i = 0; i < 4; i++) {
+                startCalendar.add(Calendar.YEAR,1);
+                endCalendar.add(Calendar.YEAR,1);
+                String start = sdf.format(startCalendar.getTime());
+                String end = sdf.format(endCalendar.getTime());
+                db = FirebaseFirestore.getInstance();
                 userInfo.put("eventName", note);
                 userInfo.put("startDate", start);
                 userInfo.put("endDate", end);
