7821a2f77e69cb23f509ac82c7c6ff09991bb614
xintian <watson050308@gmail.com>
2020-11-07 18:14:45 +0800

    modify password

--- iPets/src/components/Password.vue
+++ iPets/src/components/Password.vue
@@ -1,42 +1,88 @@
 <template>
   <b-row>
-    <b-col class="coltitle" col="4" sm="4" md="4"> <p>舊密碼：</p></b-col>
-    <b-col col="8" sm="8" md="8">
-      <b-form-input
-        class="InputClass"
-        name="oldPassword"
-        v-model="text"
-        placeholder="舊密碼"
-      ></b-form-input>
+    <b-col class="coltitle" cols="4" sm="4" md="4">
+      <p>舊密碼：</p>
     </b-col>
-    <b-col class="coltitle" col="4" sm="4" md="4"> <p>新密碼：</p></b-col>
-    <b-col col="8" sm="8" md="8">
-      <b-form-input
-        class="InputClass"
-        name="newPassword"
-        v-model="text"
-        placeholder="新密碼"
-      ></b-form-input>
+    <b-col cols="8" sm="8" md="8">
+      <b-form-input class="InputClass" name="oldPassword" v-model="oldPassword" placeholder="舊密碼"></b-form-input>
     </b-col>
-    <b-col class="coltitle" col="4" sm="4" md="4"> <p>確認新密碼：</p></b-col>
-    <b-col col="8" sm="8" md="8">
+    <b-col class="coltitle" cols="4" sm="4" md="4">
+      <p>新密碼：</p>
+    </b-col>
+    <b-col cols="8" sm="8" md="8">
+      <b-form-input class="InputClass" name="newPassword" v-model="password" placeholder="新密碼"></b-form-input>
+    </b-col>
+    <b-col class="coltitle" cols="4" sm="4" md="4">
+      <p>確認新密碼：</p>
+    </b-col>
+    <b-col cols="8" sm="8" md="8">
       <b-form-input
         class="InputClass"
         name="confirmNewPassword"
-        v-model="text"
+        v-model="check"
         placeholder="確認新密碼"
       ></b-form-input>
     </b-col>
-    <b-col col="4" sm="4" md="4"></b-col>
-    <b-col col="8" sm="8" md="8">
-      <b-button class="ButtonClass">送出</b-button>
+    <b-col cols="4" sm="4" md="4"></b-col>
+    <b-col cols="8" sm="8" md="8">
+      <b-button class="ButtonClass" @click="updatePassword">送出</b-button>
     </b-col>
   </b-row>
 </template>
 
 <script>
+import { db } from "../db";
+import firebase from "firebase";
+// const fStore = db.firestore();
+const fAuth = db.auth();
+
 export default {
-  name: "password"
+  name: "password",
+  data() {
+    return {
+      user: "",
+      oldPassword: "",
+      password: "",
+      check: ""
+    };
+  },
+  mounted() {
+    this.getPassword();
+  },
+  methods: {
+    updatePassword() {
+      if ((this.check = this.password)) {
+        var user = firebase.auth().currentUser;
+        var newPassword = fAuth.getASecureRandomPassword();
+        user
+          .updatePassword(newPassword)
+          .then(function() {
+            // Update successful.
+          })
+          .catch(function(error) {
+            alert(error);
+          });
+      }
+    },
+    getPassword() {
+      var user = firebase.auth().currentUser;
+      this.password = user.password;
+      // let uid = firebase.auth().currentUser.uid;
+      // let snapshot = fStore
+      //   .collection("users")
+      //   .doc(uid)
+      //   .get();
+      // var newPassword = getASecureRandomPassword();
+      // console.log(newPassword);
+      // const events = [];
+      // snapshot.forEach(doc => {
+      //   let appData = doc.data();
+      //   appData.id = doc.id;
+      //   events.push(appData);
+      // });
+      // fStore.
+    }
+  }
 };
 </script>
 
