4ca8b5dc96e84e9c0a0be73777d47d124e116cd4
N1066405 <n1066405@ntub.edu.tw>
2020-11-11 17:10:50 +0800

    modify photoGalleryActivity.java
    multiple image upload in firebase storage

--- app/iPets/app/src/main/java/com/example/ipets/photoGalleryActivity.java
+++ app/iPets/app/src/main/java/com/example/ipets/photoGalleryActivity.java
@@ -5,17 +5,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,15 +21,14 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.HashMap;
 
 public class photoGalleryActivity extends AppCompatActivity {
 
@@ -39,9 +36,11 @@ public class photoGalleryActivity extends AppCompatActivity {
     FloatingActionButton btnChoose; //上傳按鈕
     private GridView mGridView; //顯示手機里的所有圖片的列表控件
     private ImageView imageView;
-    private Uri filepath;
+    private Uri ImageUri;
     Button btnUpload;
+    TextView alert;
 
+    private int upload_count = 0;
     private final int PICK_IMAGE_REQUEST = 71;
 
     FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -52,10 +51,7 @@ public class photoGalleryActivity extends AppCompatActivity {
     FirebaseStorage storage;
     StorageReference storageReference;
 
-    private GridView gridView;
-    private List<String> thumbs;  //存放縮圖的id
-    private List<String> imagePaths;  //存放圖片的路徑
-    private com.example.ipets.imageAdapter imageAdapter;  //用來顯示縮圖
+    ArrayList<Uri> ImageList = new ArrayList<Uri>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,18 +67,19 @@ public class photoGalleryActivity extends AppCompatActivity {
         storageReference = storage.getReference();
 
         btnChoose = findViewById(R.id.addPhoto);
-        btnUpload = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView1);
+        btnUpload = findViewById(R.id.btnupload);
+        imageView = findViewById(R.id.photoGalleryImage1);
+        alert = findViewById(R.id.alert);
 
 
         btnChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent addPhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                addPhotoIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
                 addPhotoIntent.setType("image/*");
-                startActivityForResult(Intent.createChooser(addPhotoIntent,"Select Picture"),PICK_IMAGE_REQUEST);
-                }
+                addPhotoIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+                startActivityForResult(addPhotoIntent,PICK_IMAGE_REQUEST);
+            }
         });
 
         btnUpload.setOnClickListener(new View.OnClickListener() {
@@ -95,52 +92,64 @@ public class photoGalleryActivity extends AppCompatActivity {
     }
 
     private void uploadImage() {
-        if (filepath != null) {
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
+        StorageReference ImageFolder = storageReference.child(userUID + '/' + "Gallery/");
 
-            StorageReference ref = storageReference.child(userUID +'/' +"gallery/" + UUID.randomUUID().toString());
-            ref.putFile(filepath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        for (upload_count = 0; upload_count < ImageList.size(); upload_count++) {
+            Uri IndividualImage = ImageList.get(upload_count);
+
+            StorageReference ImageName = ImageFolder.child(IndividualImage.getLastPathSegment());
+
+            ImageName.putFile(IndividualImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    progressDialog.dismiss();
-                    Toast.makeText(photoGalleryActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    progressDialog.dismiss();
-                    Toast.makeText(photoGalleryActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
-                    progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                    ImageName.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = String.valueOf(uri);
+                            StoreLink(url);
+                            Toast.makeText(photoGalleryActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(photoGalleryActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             });
         }
     }
 
+    private void StoreLink (String url){
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("UserOne");
+            HashMap<String, String> hashMap = new HashMap<>();
+            hashMap.put("Imglink", url);
+            databaseReference.push().setValue(hashMap);
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null)
-        {
-            filepath = data.getData();
-            try{
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filepath);
-                imageView.setImageBitmap(bitmap);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
+            if (requestCode == PICK_IMAGE_REQUEST) {
+                if (resultCode == RESULT_OK) {
+                    if (data.getClipData() != null) {
+                        int countClipData = data.getClipData().getItemCount();
+                        int currentImageSelect = 0;
+                        while (currentImageSelect < countClipData) {
+                            ImageUri = data.getClipData().getItemAt(currentImageSelect).getUri();
+                            ImageList.add(ImageUri);
+                            currentImageSelect = currentImageSelect + 1;
+                        }
+                        alert.setText("You have Selected" + ImageList.size() + "Images");
+
+                    } else {
+                        Toast.makeText(this, "Please Select Mutiple Image", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         }
     }
-}
+
 
 
 
