7d675901c7c35a81720855e4cf0fd61b757757d3
xintian <watson050308@gmail.com>
2020-11-20 17:55:35 +0800

    modify manageAdoption

--- iPets/src/components/ManageAdoption.vue
+++ iPets/src/components/ManageAdoption.vue
@@ -274,7 +274,7 @@
                 <b-button
                   class="right"
                   v-on:click="$bvModal.hide(`createAdoption`); createAdoption()"
-                >確認新增文章</b-button>
+                >確認新增</b-button>
               </b-card>
             </b-col>
           </b-row>
